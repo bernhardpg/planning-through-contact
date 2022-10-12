Fix problem with jacobians

examples/one_d_pusher.py
@@ -143,8 +143,7 @@ class CollisionPair:
     body_b: RigidBody
     friction_coeff: float
     sdf: sym.Expression
-    normal_vec: npt.NDArray[np.float64]
-    tangential_vec: npt.NDArray[np.float64]
+    n_hat: npt.NDArray[np.float64]  # n_hat should be from body_a to body_b
     order: int = 2
 
     def __post_init__(self):
@@ -154,6 +153,8 @@ class CollisionPair:
         self.lam_f = BezierVariable(
             dim=1, order=self.order, name=f"{self.name}_lam_f"
         ).x
+        assert self.dim == 2  # TODO for now only works for 2D
+        self.d_hat = np.array([[-self.n_hat[1, 0]], [self.n_hat[0, 0]]])
         self.additional_constraints = []
 
     @property
@@ -171,11 +172,11 @@ class CollisionPair:
 
     @property
     def normal_jacobian(self) -> npt.NDArray[np.float64]:
-        return self.normal_vec.T.dot(self.contact_jacobian)
+        return self.n_hat.T.dot(self.contact_jacobian)
 
     @property
     def tangential_jacobian(self) -> npt.NDArray[np.float64]:
-        return self.tangential_vec.T.dot(self.contact_jacobian)
+        return self.d_hat.T.dot(self.contact_jacobian)
 
     @property
     def rel_tangential_sliding_vel(self) -> npt.NDArray[sym.Expression]:
@@ -196,17 +197,23 @@ class CollisionPair:
     def _get_jacobian_for_bodies(
         self, bodies: List[RigidBody], jacobian: npt.NDArray[np.float64]
     ) -> npt.NDArray[np.float64]:
-        body_a_idx = bodies.index(self.body_a)
-        body_b_idx = bodies.index(self.body_b)
+        # (1, num_bodies * num_dims)
+        jacobian_for_all_bodies = np.zeros((1, len(bodies) * self.dim))
 
-        body_a_cols = np.arange(body_a_idx * self.dim, (body_a_idx + 1) * self.dim)
-        body_b_cols = np.arange(body_b_idx * self.dim, (body_b_idx + 1) * self.dim)
-        jacobian_in_all_bodies = np.zeros((1, len(bodies) * self.dim))
-        jacobian_in_all_bodies[:, body_a_cols] = jacobian[:, 0 : 1 * self.dim]
-        jacobian_in_all_bodies[:, body_b_cols] = jacobian[
-            :, 1 * self.dim : 2 * self.dim
+        body_a_idx_in_J = bodies.index(self.body_a) * self.dim
+        body_b_idx_in_J = bodies.index(self.body_b) * self.dim
+        body_a_cols_in_J = np.arange(body_a_idx_in_J, body_a_idx_in_J + self.dim)
+        body_b_cols_in_J = np.arange(body_b_idx_in_J, body_b_idx_in_J + self.dim)
+        body_a_cols_in_local_J = np.arange(0, self.dim)
+        body_b_cols_in_local_J = np.arange(self.dim, 2 * self.dim)
+
+        jacobian_for_all_bodies[:, body_a_cols_in_J] = jacobian[
+            :, body_a_cols_in_local_J
         ]
-        return jacobian_in_all_bodies
+        jacobian_for_all_bodies[:, body_b_cols_in_J] = jacobian[
+            :, body_b_cols_in_local_J
+        ]
+        return jacobian_for_all_bodies
 
     def add_constraint_to_all_modes(self, constraints) -> None:
         self.additional_constraints = sum(
@@ -296,27 +303,25 @@ def plan_for_one_d_pusher_2():
 
     # NOTE this is the stuff the jacobians will replace
     sdf_finger_box = x_b - x_f - box_width
-    sdf_box_ground = y_b - y_g - box_height
+    sdf_ground_box = y_b - y_g - box_height
 
     pair_finger_box = CollisionPair(
         finger,
         box,
         friction_coeff,
         sdf_finger_box,
-        normal_vec=np.array([[1], [0]]),
-        tangential_vec=np.array([[0], [1]]),
+        n_hat=np.array([[1], [0]]),
     )
-    pair_box_ground = CollisionPair(
-        box,
+    pair_ground_box = CollisionPair(
         ground,
+        box,
         friction_coeff,
-        sdf_box_ground,
-        normal_vec=np.array([[0], [1]]),
-        tangential_vec=np.array([[-1], [0]]),
+        sdf_ground_box,
+        n_hat=np.array([[0], [1]]),
     )
 
     all_bodies = [finger, box, ground]
-    all_pairs = [pair_finger_box, pair_box_ground]
+    all_pairs = [pair_finger_box, pair_ground_box]
 
     normal_jacobians = np.vstack(
         [p.get_normal_jacobian_for_bodies(all_bodies) for p in all_pairs]
@@ -328,37 +333,37 @@ def plan_for_one_d_pusher_2():
     normal_forces = np.concatenate([p.lam_n for p in all_pairs])
     friction_forces = np.concatenate([p.lam_f for p in all_pairs])
 
-    # TODO generalize this
-    gravitational_jacobian = np.array([[0, -1, 0, -1, 0, -1]]).T
+    # Gravity only affects unactuated objects
+    gravitational_jacobian = np.array([[0, 0, 0, -1, 0, 0]]).T
 
     all_force_balances = eq(
         normal_jacobians.T.dot(normal_forces)
-        + tangential_jacobians.T.dot(friction_forces),
-        gravitational_jacobian.dot(mg),
+        + tangential_jacobians.T.dot(friction_forces)
+        + gravitational_jacobian.dot(mg),
+        0,
     )
 
     unactuated_dofs = (2, 3)
     force_balance = all_force_balances[unactuated_dofs, :]
 
     #    force_balance = [
-    #        eq(pair_finger_box.lam_n, -pair_box_ground.lam_f),
-    #        eq(pair_box_ground.lam_n, -pair_finger_box.lam_f + mg),
+    #        eq(pair_finger_box.lam_n, -pair_ground_box.lam_f),
+    #        eq(pair_ground_box.lam_n, -pair_finger_box.lam_f + mg),
     #    ]
-    breakpoint()
-    pair_finger_box.add_force_balance(force_balance)
-    pair_box_ground.add_force_balance(force_balance)
+    for p in all_pairs:
+        p.add_force_balance(force_balance)
 
     no_ground_motion = [eq(x_g, 0), eq(y_g, 0)]
     no_box_y_motion = eq(y_b, box_height)
     finger_pos_below_box_height = le(y_f, y_b + box_height)
     additional_constraints_finger_box = [*no_ground_motion, finger_pos_below_box_height]
-    additional_constraints_box_ground = [
+    additional_constraints_ground_box = [
         *no_ground_motion,
         no_box_y_motion,
-        eq(pair_box_ground.lam_n, mg),
+        eq(pair_ground_box.lam_n, mg),
     ]
     pair_finger_box.add_constraint_to_all_modes(additional_constraints_finger_box)
-    pair_box_ground.add_constraint_to_all_modes(additional_constraints_box_ground)
+    pair_ground_box.add_constraint_to_all_modes(additional_constraints_ground_box)
 
     all_variables = np.concatenate(
         [
@@ -370,15 +375,15 @@ def plan_for_one_d_pusher_2():
             y_g.flatten(),
             pair_finger_box.lam_n.flatten(),
             pair_finger_box.lam_f.flatten(),
-            pair_box_ground.lam_n.flatten(),
-            pair_box_ground.lam_f.flatten(),
+            pair_ground_box.lam_n.flatten(),
+            pair_ground_box.lam_f.flatten(),
         ]
     )
 
     pair_finger_box.create_contact_modes(all_variables)
-    pair_box_ground.create_contact_modes(all_variables)
+    pair_ground_box.create_contact_modes(all_variables)
 
-    contact_pairs = [pair_finger_box.contact_modes, pair_box_ground.contact_modes]
+    contact_pairs = [pair_finger_box.contact_modes, pair_ground_box.contact_modes]
 
     possible_contact_permutations = itertools.product(*contact_pairs)
     convex_sets = {
@@ -474,6 +479,7 @@ def plan_for_one_d_pusher_2():
     options.preprocessing = True
     options.max_rounded_paths = 10
 
+    # Create diagram
     graphviz = gcs.GetGraphvizString()
     data = pydot.graph_from_dot_data(graphviz)[0]
     data.write_svg("graph.svg")
@@ -482,6 +488,7 @@ def plan_for_one_d_pusher_2():
     assert result.is_success()
     print("Result is success!")
 
+    # Save solution diagram
     graphviz = gcs.GetGraphvizString(result, False, precision=1)
     data = pydot.graph_from_dot_data(graphviz)[0]
     data.write_svg("graph_solution.svg")
@@ -507,15 +514,15 @@ def plan_for_one_d_pusher_2():
         VariableMetadata(1, 2, "ground_pos_y"),
         VariableMetadata(1, 2, "finger_box_normal_force"),
         VariableMetadata(1, 2, "finger_box_friction_force"),
-        VariableMetadata(1, 2, "box_ground_normal_force"),
-        VariableMetadata(1, 2, "box_ground_friction_force"),
+        VariableMetadata(1, 2, "ground_box_normal_force"),
+        VariableMetadata(1, 2, "ground_box_friction_force"),
     ]
 
     # Create Bezier Curve
     curves = evaluate_curve_from_ctrl_points(vertex_values, var_mds)
 
+    # Plot
     plt.plot(np.hstack(list(curves.values())))
     plt.legend(list(curves.keys()))
     animate_2d_box(**curves, box_width=box_width, box_height=box_height)
-
     return

visualize/visualize.py
@@ -12,8 +12,8 @@ def animate_2d_box(
     ground_pos_y,
     finger_box_normal_force,
     finger_box_friction_force,
-    box_ground_normal_force,
-    box_ground_friction_force,
+    ground_box_normal_force,
+    ground_box_friction_force,
     box_width,
     box_height,
 ):
@@ -25,8 +25,8 @@ def animate_2d_box(
     (finger,) = ax.plot([], [], "bo", lw=10)
     (finger_box_normal_force_plot,) = ax.plot([], [], "g>-", lw=2)
     (finger_box_friction_force_plot,) = ax.plot([], [], "g^-", lw=2)
-    (box_ground_normal_force_plot,) = ax.plot([], [], "c^-", lw=2)
-    (box_ground_friction_force_plot,) = ax.plot([], [], "c<-", lw=2)
+    (ground_box_normal_force_plot,) = ax.plot([], [], "c^-", lw=2)
+    (ground_box_friction_force_plot,) = ax.plot([], [], "c<-", lw=2)
     (ground,) = ax.plot([-100, 100], [0, 0])
 
     # initialization function: plot the background of each frame
@@ -35,15 +35,15 @@ def animate_2d_box(
         box.set_data([], [])
         finger_box_normal_force_plot.set_data([], [])
         finger_box_friction_force_plot.set_data([], [])
-        box_ground_normal_force_plot.set_data([], [])
-        box_ground_friction_force_plot.set_data([], [])
+        ground_box_normal_force_plot.set_data([], [])
+        ground_box_friction_force_plot.set_data([], [])
         return (
             finger,
             box,
             finger_box_normal_force_plot,
             finger_box_friction_force_plot,
-            box_ground_normal_force_plot,
-            box_ground_friction_force_plot,
+            ground_box_normal_force_plot,
+            ground_box_friction_force_plot,
         )
 
     # animation function.  This is called sequentially
@@ -76,19 +76,19 @@ def animate_2d_box(
             ],
             finger_pos_y[i],
         )
-        box_ground_normal_force_plot.set_data(
+        ground_box_normal_force_plot.set_data(
             box_pos_x[i],
             [
                 box_pos_y[i] - box_height,
-                box_pos_y[i] - box_height + box_ground_normal_force[i],
+                box_pos_y[i] - box_height + ground_box_normal_force[i],
             ],
         )
         finger_box_friction_force_plot.set_data(
             box_pos_x[i] - box_width,
             [finger_pos_y[i], finger_pos_y[i] + finger_box_friction_force[i]],
         )
-        box_ground_friction_force_plot.set_data(
-            [box_pos_x[i], box_pos_x[i] + box_ground_friction_force[i]],
+        ground_box_friction_force_plot.set_data(
+            [box_pos_x[i], box_pos_x[i] + ground_box_friction_force[i]],
             box_pos_y[i] - box_height,
         )
 
@@ -97,59 +97,12 @@ def animate_2d_box(
             box,
             finger_box_normal_force_plot,
             finger_box_friction_force_plot,
-            box_ground_normal_force_plot,
-            box_ground_friction_force_plot,
+            ground_box_normal_force_plot,
+            ground_box_friction_force_plot,
         )
 
     # call the animator.  blit=True means only re-draw the parts that have changed.
-    anim = animation.FuncAnimation(
-        fig, animate, init_func=init, frames=n_frames, interval=20, blit=True
-    )
-
-    plt.show()
-
-
-def animate_1d_box(x_f, x_b, lam_n, lam_f):
-    n_frames = x_f.shape[0]
-    # First set up the figure, the axis, and the plot element we want to animate
-    fig = plt.figure()
-    ax = plt.axes(xlim=(-4, 10), ylim=(-2, 4))
-    (box,) = ax.plot([], [], "r", lw=5)
-    (finger,) = ax.plot([], [], "bo", lw=10)
-    (force_normal,) = ax.plot([], [], "g>-", lw=2)
-    (force_friction,) = ax.plot([], [], "g<-", lw=2)
-    (ground,) = ax.plot([-100, 100], [0, 0])
-
-    # initialization function: plot the background of each frame
-    def init():
-        finger.set_data([], [])
-        box.set_data([], [])
-        force_normal.set_data([], [])
-        force_friction.set_data([], [])
-        return (finger, box, force_normal, force_friction)
-
-    # animation function.  This is called sequentially
-    def animate(i):
-        l = 2.0  # TODO
-        height = 1.0  # TODO
-        y = 0.0
-        finger_height = y + 0.5
-
-        box_com = x_b[i]
-        box_shape_x = np.array(
-            [box_com - l, box_com + l, box_com + l, box_com - l, box_com - l]
-        )
-        box_shape_y = np.array([y, y, y + height, y + height, y])
-        box.set_data(box_shape_x, box_shape_y)
-
-        force_normal.set_data([box_com - l, box_com - l + lam_n[i]], finger_height)
-        force_friction.set_data([box_com, box_com + lam_f[i]], y)
-        finger.set_data(x_f[i], finger_height)
-
-        return (finger, box, force_normal, force_friction)
-
-    # call the animator.  blit=True means only re-draw the parts that have changed.
-    anim = animation.FuncAnimation(
+    animation.FuncAnimation(
         fig, animate, init_func=init, frames=n_frames, interval=20, blit=True
     )
 