53bdde87a963cf477ea2c08113d5bc3dcdc8130d
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2022-10-12 22:42:59 -0400

    Add demo to run as cmd line argument to main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+
 from examples.one_d_pusher import (
     plan_for_one_box_one_finger,
     plan_for_one_finger_two_boxes,
@@ -6,12 +8,16 @@ from examples.one_d_pusher import (
 
 
 def main():
-    plan = "one_finger_one_box"
-    if plan == "one_finger_one_box":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--demo", help="Demo to run", type=str, default="two_fingers")
+    args = parser.parse_args()
+    demo_to_run = args.demo
+
+    if demo_to_run in "one_finger_one_box":
         plan_for_one_box_one_finger()
-    elif plan == "two_fingers":
+    elif demo_to_run in "two_fingers":
         plan_for_two_fingers()
-    elif plan == "two_boxes":
+    elif demo_to_run in "two_boxes":
         plan_for_one_finger_two_boxes()
     return 0
 
