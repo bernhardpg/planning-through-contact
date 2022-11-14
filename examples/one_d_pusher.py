51863e8b499906cad48282a1a91d490b240940f1
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2022-11-14 09:30:00 +0100

    refactor gcs.py to contact_planner.py

--- examples/one_d_pusher.py
+++ examples/one_d_pusher.py
@@ -1,11 +1,10 @@
 import numpy as np
 from pydrake.math import eq
 
-from geometry.bezier import BezierCurve
 from geometry.collision_pair import CollisionPair
 from geometry.contact_mode import ContactModeType, PositionModeType
 from geometry.rigid_body import RigidBody
-from planning.gcs import GcsContactPlanner
+from planning.contact_planner import GcsContactPlanner
 from planning.graph_builder import ContactModeConfig
 from visualize.visualize import animate_positions, plot_positions_and_forces
 
