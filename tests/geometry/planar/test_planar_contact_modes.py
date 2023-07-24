8bcb8cf6daba5e6a0cc0da88d6e810f8a2652343
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2023-07-24 13:12:38 +0200

    Use fixtures

--- tests/geometry/planar/test_planar_contact_modes.py
+++ tests/geometry/planar/test_planar_contact_modes.py
@@ -1,4 +1,4 @@
-# import pytest
+import pytest
 from pydrake.solvers import MathematicalProgram
 from pydrake.symbolic import Expression, Variables
 
@@ -15,10 +15,15 @@ from geometry.planar.planar_contact_modes import (
 from geometry.rigid_body import RigidBody
 
 
-def test_face_contact_variables() -> None:
+@pytest.fixture
+def box_geometry() -> Box2d:
+    return Box2d(width=0.2, height=0.1)
+
+
+@pytest.fixture
+def face_contact_vars(box_geometry: Box2d) -> FaceContactVariables:
     prog = MathematicalProgram()
-    box_geometry = Box2d(width=0.2, height=0.1)
-    contact_location = PolytopeContactLocation(ContactLocation.FACE, 0)
+    contact_location = PolytopeContactLocation(ContactLocation.FACE, 3)
 
     num_knot_points = 4
     time_in_contact = 2
@@ -30,65 +35,71 @@ def test_face_contact_variables() -> None:
         num_knot_points,
         time_in_contact,
     )
+    return vars
 
-    assert vars.lams.shape == (num_knot_points,)
-    assert vars.normal_forces.shape == (num_knot_points,)
-    assert vars.friction_forces.shape == (num_knot_points,)
-    assert vars.cos_ths.shape == (num_knot_points,)
-    assert vars.sin_ths.shape == (num_knot_points,)
-    assert vars.p_WB_xs.shape == (num_knot_points,)
-    assert vars.p_WB_ys.shape == (num_knot_points,)
 
-    assert all(vars.pv1 == box_geometry.vertices[0])
-    assert all(vars.pv2 == box_geometry.vertices[1])
+def test_face_contact_variables(
+    box_geometry: Box2d, face_contact_vars: FaceContactVariables
+) -> None:
+    num_knot_points = face_contact_vars.num_knot_points
 
-    assert len(vars.R_WBs) == num_knot_points
-    for R in vars.R_WBs:
+    assert face_contact_vars.lams.shape == (num_knot_points,)
+    assert face_contact_vars.normal_forces.shape == (num_knot_points,)
+    assert face_contact_vars.friction_forces.shape == (num_knot_points,)
+    assert face_contact_vars.cos_ths.shape == (num_knot_points,)
+    assert face_contact_vars.sin_ths.shape == (num_knot_points,)
+    assert face_contact_vars.p_WB_xs.shape == (num_knot_points,)
+    assert face_contact_vars.p_WB_ys.shape == (num_knot_points,)
+
+    assert all(face_contact_vars.pv1 == box_geometry.vertices[3])
+    assert all(face_contact_vars.pv2 == box_geometry.vertices[0])
+
+    assert len(face_contact_vars.R_WBs) == num_knot_points
+    for R in face_contact_vars.R_WBs:
         assert R.shape == (2, 2)
 
-    assert len(vars.p_WBs) == num_knot_points
-    for p in vars.p_WBs:
+    assert len(face_contact_vars.p_WBs) == num_knot_points
+    for p in face_contact_vars.p_WBs:
         assert p.shape == (2, 1)
 
-    assert len(vars.f_c_Bs) == num_knot_points
-    for f in vars.f_c_Bs:
+    assert len(face_contact_vars.f_c_Bs) == num_knot_points
+    for f in face_contact_vars.f_c_Bs:
         assert f.shape == (2, 1)
 
-    assert len(vars.p_c_Bs) == num_knot_points
-    for p_c in vars.p_c_Bs:
+    assert len(face_contact_vars.p_c_Bs) == num_knot_points
+    for p_c in face_contact_vars.p_c_Bs:
         assert p_c.shape == (2, 1)
 
-    assert len(vars.v_WBs) == num_knot_points - 1
-    for v in vars.v_WBs:
+    assert len(face_contact_vars.v_WBs) == num_knot_points - 1
+    for v in face_contact_vars.v_WBs:
         assert v.shape == (2, 1)
 
-    assert len(vars.cos_th_dots) == num_knot_points - 1
-    for c in vars.cos_th_dots:
+    assert len(face_contact_vars.cos_th_dots) == num_knot_points - 1
+    for c in face_contact_vars.cos_th_dots:
         assert isinstance(c, Expression)
 
-    assert len(vars.sin_th_dots) == num_knot_points - 1
-    for s in vars.cos_th_dots:
+    assert len(face_contact_vars.sin_th_dots) == num_knot_points - 1
+    for s in face_contact_vars.cos_th_dots:
         assert isinstance(s, Expression)
 
-    assert len(vars.v_c_Bs) == num_knot_points - 1
-    for v in vars.v_c_Bs:
+    assert len(face_contact_vars.v_c_Bs) == num_knot_points - 1
+    for v in face_contact_vars.v_c_Bs:
         assert v.shape == (2, 1)
 
-    assert len(vars.omega_WBs) == num_knot_points - 1
-    for o in vars.omega_WBs:
+    assert len(face_contact_vars.omega_WBs) == num_knot_points - 1
+    for o in face_contact_vars.omega_WBs:
         assert isinstance(o, Expression)
 
-    assert len(vars.p_c_Ws) == num_knot_points
-    for p in vars.p_c_Ws:
+    assert len(face_contact_vars.p_c_Ws) == num_knot_points
+    for p in face_contact_vars.p_c_Ws:
         assert p.shape == (2, 1)
 
-    assert len(vars.f_c_Ws) == num_knot_points
-    for f in vars.f_c_Ws:
+    assert len(face_contact_vars.f_c_Ws) == num_knot_points
+    for f in face_contact_vars.f_c_Ws:
         assert f.shape == (2, 1)
 
 
-def test_face_contact_mode() -> None:
-    box_geometry = Box2d(width=0.1, height=0.2)
+def test_face_contact_mode(box_geometry: Box2d) -> None:
     mass = 0.1
     box = RigidBody("box", box_geometry, mass)
     contact_location = PolytopeContactLocation(
@@ -116,45 +127,36 @@ def test_face_contact_mode() -> None:
     )
 
 
-def test_quasi_static_dynamics() -> None:
-    prog = MathematicalProgram()
-    box_geometry = Box2d(width=0.2, height=0.1)
+def test_quasi_static_dynamics(face_contact_vars: FaceContactVariables) -> None:
     mass = 0.1
     friction_coeff = 0.5
-    contact_location = PolytopeContactLocation(ContactLocation.FACE, 3)
-
-    num_knot_points = 4
-    time_in_contact = 2
-
-    vars = FaceContactVariables.from_prog(
-        prog,
-        box_geometry,
-        contact_location,
-        num_knot_points,
-        time_in_contact,
-    )
 
     k = 0
 
-    f_c_B = vars.f_c_Bs[k]
-    p_c_B = vars.p_c_Bs[k]
-    R_WB = vars.R_WBs[k]
-    v_WB = vars.v_WBs[k]
-    omega_WB = vars.omega_WBs[k]
+    f_c_B = face_contact_vars.f_c_Bs[k]
+    p_c_B = face_contact_vars.p_c_Bs[k]
+    R_WB = face_contact_vars.R_WBs[k]
+    v_WB = face_contact_vars.v_WBs[k]
+    omega_WB = face_contact_vars.omega_WBs[k]
 
-    x_dot, dyn = FaceContactMode.quasi_static_dynamics(
+    _, dyn = FaceContactMode.quasi_static_dynamics(
         v_WB, omega_WB, f_c_B, p_c_B, R_WB, friction_coeff, mass
     )
 
     check_vars_eq = lambda e, v: e.GetVariables().EqualTo(Variables(v))
-    assert check_vars_eq(dyn[0], [vars.normal_forces[0]])
-    assert check_vars_eq(dyn[1], [vars.friction_forces[0]])
+    assert check_vars_eq(dyn[0], [face_contact_vars.normal_forces[0]])
+    assert check_vars_eq(dyn[1], [face_contact_vars.friction_forces[0]])
     assert check_vars_eq(
-        dyn[2], [vars.lams[0], vars.normal_forces[0], vars.friction_forces[0]]
+        dyn[2],
+        [
+            face_contact_vars.lams[0],
+            face_contact_vars.normal_forces[0],
+            face_contact_vars.friction_forces[0],
+        ],
     )
 
 
-if __name__ == "__main__":
-    # test_face_contact_variables()
-    test_face_contact_mode()
-    # test_quasi_static_dynamics()
+# if __name__ == "__main__":
+#     # test_face_contact_variables()
+#     # test_face_contact_mode(box_geometry())
+#     test_quasi_static_dynamics(face_contact_vars(box_geometry()))
