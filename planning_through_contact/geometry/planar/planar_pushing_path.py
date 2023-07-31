8ffd20214dcb1da2bbb1a1b122a13ceaaa1ef100
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2023-07-31 10:01:33 +0200

    Test rounding for a single contact mode

--- planning_through_contact/geometry/planar/planar_pushing_path.py
+++ planning_through_contact/geometry/planar/planar_pushing_path.py
@@ -13,7 +13,10 @@ from pydrake.solvers import (
     Solve,
 )
 
-from planning_through_contact.geometry.planar.abstract_mode import AbstractModeVariables
+from planning_through_contact.geometry.planar.abstract_mode import (
+    AbstractContactMode,
+    AbstractModeVariables,
+)
 from planning_through_contact.geometry.planar.non_collision_subgraph import (
     VertexModePair,
 )
@@ -25,6 +28,63 @@ from planning_through_contact.tools.types import NpVariableArray
 
 GcsVertex = opt.GraphOfConvexSets.Vertex
 GcsEdge = opt.GraphOfConvexSets.Edge
+
+
+def assemble_progs_from_contact_modes(
+    modes: List[AbstractContactMode],
+) -> MathematicalProgram:
+    prog = MathematicalProgram()
+
+    for mode in modes:
+        mode_prog = mode.prog
+
+        vars = mode_prog.decision_variables()
+        prog.AddDecisionVariables(vars)
+
+        for c in mode_prog.GetAllConstraints():
+            prog.AddConstraint(c.evaluator(), c.variables())
+
+        for c in mode_prog.GetAllCosts():
+            prog.AddCost(c.evaluator(), c.variables())
+
+    return prog
+
+
+def get_mode_variables_from_constraint_variables(
+    constraint_vars: NpVariableArray, pair_u: VertexModePair, pair_v: VertexModePair
+) -> NpVariableArray:
+    num_vars_u = pair_u.mode.prog.num_vars()
+    num_vars_v = pair_v.mode.prog.num_vars()
+
+    # don't add variables that are not in the modes (these come from the
+    # SDP relaxation)
+    vertex_vars_stacked = np.concatenate(
+        (pair_u.vertex.x()[:num_vars_u], pair_v.vertex.x()[:num_vars_v])
+    )
+
+    # find indices by constructing a new mock program
+    temp = MathematicalProgram()
+    temp.AddDecisionVariables(vertex_vars_stacked)
+    idxs = temp.FindDecisionVariableIndices(constraint_vars)
+
+    mode_vars_stacked = np.concatenate(
+        (
+            pair_u.mode.prog.decision_variables(),
+            pair_v.mode.prog.decision_variables(),
+        )
+    )
+    return mode_vars_stacked[idxs]
+
+
+def add_edge_constraints_to_prog(
+    edges: List[GcsEdge], prog: MathematicalProgram, pairs: List[VertexModePair]
+) -> None:
+    for edge, (pair_u, pair_v) in zip(edges, zip(pairs[:-1], pairs[1:])):
+        for c in edge.GetConstraints():
+            vars = get_mode_variables_from_constraint_variables(
+                c.variables(), pair_u, pair_v
+            )
+            prog.AddConstraint(c.evaluator(), vars)
 
 
 class PlanarPushingPath:
@@ -84,57 +144,9 @@ class PlanarPushingPath:
     def get_vertices(self) -> List[GcsVertex]:
         return [p.vertex for p in self.pairs]
 
-    # TODO(bernhardpg): move somewhere more suitable
-    @staticmethod
-    def get_mode_variables_from_constraint_variables(
-        constraint_vars: NpVariableArray, pair_u: VertexModePair, pair_v: VertexModePair
-    ) -> NpVariableArray:
-        num_vars_u = pair_u.mode.prog.num_vars()
-        num_vars_v = pair_v.mode.prog.num_vars()
-
-        # don't add variables that are not in the modes (these come from the
-        # SDP relaxation)
-        vertex_vars_stacked = np.concatenate(
-            (pair_u.vertex.x()[:num_vars_u], pair_v.vertex.x()[:num_vars_v])
-        )
-
-        # find indices by constructing a new mock program
-        temp = MathematicalProgram()
-        temp.AddDecisionVariables(vertex_vars_stacked)
-        idxs = temp.FindDecisionVariableIndices(constraint_vars)
-
-        mode_vars_stacked = np.concatenate(
-            (
-                pair_u.mode.prog.decision_variables(),
-                pair_v.mode.prog.decision_variables(),
-            )
-        )
-        return mode_vars_stacked[idxs]
-
     def _construct_nonlinear_program(self) -> MathematicalProgram:
-        prog = MathematicalProgram()
-
-        for p in self.pairs:
-            mode_prog = p.mode.prog
-
-            vars = mode_prog.decision_variables()
-            prog.AddDecisionVariables(vars)
-
-            for c in mode_prog.GetAllConstraints():
-                prog.AddConstraint(c.evaluator(), c.variables())
-
-            for c in mode_prog.GetAllCosts():
-                prog.AddCost(c.evaluator(), c.variables())
-
-        for edge, (pair_u, pair_v) in zip(
-            self.edges, zip(self.pairs[:-1], self.pairs[1:])
-        ):
-            for c in edge.GetConstraints():
-                vars = self.get_mode_variables_from_constraint_variables(
-                    c.variables(), pair_u, pair_v
-                )
-                prog.AddConstraint(c.evaluator(), vars)
-
+        prog = assemble_progs_from_contact_modes([p.mode for p in self.pairs])
+        add_edge_constraints_to_prog(self.edges, prog, self.pairs)
         return prog
 
     def _get_initial_guess(self) -> npt.NDArray[np.float64]:
@@ -145,7 +157,6 @@ class PlanarPushingPath:
                 for pair, num_vars in zip(self.pairs, num_vars_in_modes)
             ]
         )
-        breakpoint()
         vertex_var_vals = self.result.GetSolution(all_vertex_vars_concatenated)
         return vertex_var_vals
 
--- tests/geometry/planar/test_planar_pushing_path.py
+++ tests/geometry/planar/test_planar_pushing_path.py
@@ -1,16 +1,63 @@
+import numpy as np
 import pytest
+from pydrake.solvers import Solve
 
+from planning_through_contact.geometry.planar.face_contact import FaceContactMode
 from planning_through_contact.geometry.planar.planar_pose import PlanarPose
+from planning_through_contact.geometry.planar.planar_pushing_path import (
+    assemble_progs_from_contact_modes,
+)
 from planning_through_contact.geometry.planar.trajectory_builder import (
     PlanarTrajectoryBuilder,
 )
 from planning_through_contact.planning.planar.planar_pushing_planner import (
     PlanarPushingPlanner,
 )
+from planning_through_contact.visualize.analysis import plot_cos_sine_trajs
 from planning_through_contact.visualize.planar import (
     visualize_planar_pushing_trajectory,
 )
-from tests.geometry.planar.fixtures import box_geometry, planner, rigid_body_box
+from tests.geometry.planar.fixtures import (
+    box_geometry,
+    face_contact_mode,
+    planner,
+    rigid_body_box,
+)
+from tests.geometry.planar.tools import assert_initial_and_final_poses
+
+
+def test_rounding_one_mode(face_contact_mode: FaceContactMode) -> None:
+    initial_pose = PlanarPose(0, 0, 0)
+    final_pose = PlanarPose(0.3, 0, 0.8)
+    face_contact_mode.set_slider_initial_pose(initial_pose)
+    face_contact_mode.set_slider_final_pose(final_pose)
+
+    face_contact_mode.formulate_convex_relaxation()
+
+    assert face_contact_mode.relaxed_prog is not None
+
+    relaxed_result = Solve(face_contact_mode.relaxed_prog)
+    assert relaxed_result.is_success()
+
+    prog = assemble_progs_from_contact_modes([face_contact_mode])
+    initial_guess = relaxed_result.GetSolution(
+        face_contact_mode.relaxed_prog.decision_variables()[: prog.num_vars()]
+    )
+
+    prog.SetInitialGuess(prog.decision_variables(), initial_guess)
+    result = Solve(prog)
+
+    vars = face_contact_mode.variables.eval_result(result)
+    traj = PlanarTrajectoryBuilder([vars]).get_trajectory(interpolate=False)
+
+    assert_initial_and_final_poses(traj, initial_pose, None, final_pose, None)
+
+    DEBUG = False
+    if DEBUG:
+        visualize_planar_pushing_trajectory(traj, face_contact_mode.object.geometry)
+        # (num_knot_points, 2): first col cosines, second col sines
+        rs = np.vstack([R_WB[:, 0] for R_WB in traj.R_WB])
+        plot_cos_sine_trajs(rs)
 
 
 @pytest.mark.parametrize(
@@ -40,7 +87,7 @@ def test_path_with_teleportation(planner: PlanarPushingPlanner) -> None:
     # We should always have one more vertex than edge in a solution
     assert len(path.pairs) == len(path.edges) + 1
 
-    prog = path._construct_nonlinear_program_from_path()
+    prog = path._construct_nonlinear_program()
 
     expected_num_vars = sum([p.mode.prog.num_vars() for p in path.pairs])
     assert prog.num_vars() == expected_num_vars
