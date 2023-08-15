0d6074a2f8d3c7ddd9f0b90e84ec200e558156e8
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2023-08-15 15:43:19 +0200

    Add comment on slow solve times

--- planning_through_contact/experiments/planar_pushing/one_contact_mode.py
+++ planning_through_contact/experiments/planar_pushing/one_contact_mode.py
@@ -570,7 +570,7 @@ def plan_planar_pushing(
     # Solve the problem by using elimination of equality constraints
     eliminate_equalities = True
 
-    NUM_TRIALS = 1
+    NUM_TRIALS = 5
     DEBUG = False
 
     solver_options = SolverOptions()
@@ -583,7 +583,9 @@ def plan_planar_pushing(
             smaller_prog, retrieve_x = eliminate_equality_constraints(
                 contact_mode.prog, print_num_vars_eliminated=True
             )
+            # TODO(bernhardpg): This is consistently slower to solve than my implementation
             relaxed_prog = MakeSemidefiniteRelaxation(smaller_prog)
+            # relaxed_prog, _, _ = create_sdp_relaxation(smaller_prog)
         else:
             relaxed_prog = contact_mode.relaxed_prog
 
