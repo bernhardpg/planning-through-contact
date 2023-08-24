b17b1c34f73ff0a58a42f8e216bd753ed0ede78f
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2023-08-24 14:55:26 +0200

    Add trajectory publisher to position controller

--- planning_through_contact/simulation/planar_pushing/planar_pushing_position_control.py
+++ planning_through_contact/simulation/planar_pushing/planar_pushing_position_control.py
@@ -1,4 +1,6 @@
 import numpy as np
+from pydrake.common.value import AbstractValue
+from pydrake.math import RigidTransform
 from pydrake.multibody.inverse_kinematics import (
     DifferentialInverseKinematicsIntegrator,
     DifferentialInverseKinematicsParameters,
@@ -10,15 +12,46 @@ from pydrake.multibody.parsing import (
 )
 from pydrake.multibody.plant import MultibodyPlant
 from pydrake.systems.analysis import Simulator
-from pydrake.systems.framework import DiagramBuilder
+from pydrake.systems.framework import Context, DiagramBuilder, LeafSystem
 from pyparsing import Path
 
+from planning_through_contact.geometry.planar.planar_pose import PlanarPose
 from planning_through_contact.geometry.planar.planar_pushing_trajectory import (
     PlanarPushingTrajectory,
 )
 from planning_through_contact.simulation.hardware.hardware_interface import (
     ManipulationHardwareInterface,
 )
+
+
+class PusherPosePublisher(LeafSystem):
+    def __init__(
+        self,
+        traj: PlanarPushingTrajectory,
+        z_dist_to_table: float = 0.5,
+        delay_before_start: float = 10,
+    ):
+        super().__init__()
+        self.traj = traj
+        self.z_dist = z_dist_to_table
+        self.delay = delay_before_start
+
+        self.DeclareAbstractOutputPort(
+            "pose", lambda: AbstractValue.Make(RigidTransform()), self.DoCalcOutput
+        )
+
+    def _calc_pose(self, t: float) -> RigidTransform:
+        p_c_W = self.traj.get_value(0, "p_c_W")
+        assert isinstance(p_c_W, type(np.array([])))
+
+        planar_pose = PlanarPose(p_c_W[0].item(), p_c_W[1].item(), theta=0)
+        return planar_pose.to_pose(object_height=self.z_dist)
+
+    def DoCalcOutput(self, context: Context, output):
+        curr_t = context.get_time()
+        end_effector_pose = self._calc_pose(curr_t)
+        print(end_effector_pose)
+        output.set_value(end_effector_pose)
 
 
 class PlanarPushingPositionControl:
@@ -30,30 +63,33 @@ class PlanarPushingPositionControl:
     def __init__(self, traj: PlanarPushingTrajectory):
         builder = DiagramBuilder()
 
-        self.station = builder.AddSystem(ManipulationHardwareInterface())
-        self.station.Connect()
+        self.hardware_interface = builder.AddNamedSystem(
+            "hardware_interface",
+            ManipulationHardwareInterface(),
+        )
+        self.hardware_interface.Connect()
 
         self._load_robot()
         self._add_ik_system(builder)
 
-        # TODO(bernhardpg): Make traj publisher
-        self._connect_traj_publisher(builder)
+        self._add_pose_publisher(builder, traj)
 
-        diagram = builder.Build()
-        self.simulator = Simulator(diagram)
+        self.diagram = builder.Build()
+
+        self.simulator = Simulator(self.diagram)
 
         # This is important to avoid duplicate publishes to the hardware interface:
         self.simulator.set_publish_every_time_step(False)
 
         self.simulator.set_target_realtime_rate(1.0)
 
-        self.station_context = diagram.GetMutableSubsystemContext(
-            self.station, self.simulator.get_mutable_context()
+        self.hardware_interface_context = self.diagram.GetMutableSubsystemContext(
+            self.hardware_interface, self.simulator.get_mutable_context()
         )
 
         # TODO(bernhardpg): Do we want to compute a feedforward torque?
-        self.station.GetInputPort("iiwa_feedforward_torque").FixValue(
-            self.station_context, np.zeros(7)
+        self.hardware_interface.GetInputPort("iiwa_feedforward_torque").FixValue(
+            self.hardware_interface_context, np.zeros(7)
         )
 
         # If the diagram is only the hardware interface, then we must advance it a
@@ -61,7 +97,9 @@ class PlanarPushingPositionControl:
         # already publishing correct positions even without advancing, and indeed
         # we must not advance a simulated plant until the sliders and filters have
         # been initialized to match the plant.
-        self.simulator.AdvanceTo(1e-6)
+        real_hardware = True  # TODO(bernhardpg)
+        if real_hardware:
+            self.simulator.AdvanceTo(1e-6)
 
         self._initialize_ik()
 
@@ -84,10 +122,10 @@ class PlanarPushingPositionControl:
 
         time_step = 0.005
         ik_params.set_time_step(time_step)
-        # True velocity limits for the IIWA14 (in rad, rounded down to the first
-        # decimal)
+        # True velocity limits for the IIWA14
+        # (in rad, rounded down to the first decimal)
         iiwa14_velocity_limits = np.array([1.4, 1.4, 1.7, 1.3, 2.2, 2.3, 2.3])
-        velocity_limit_factor = 1.0
+        velocity_limit_factor = 0.1
         ik_params.set_joint_velocity_limits(
             (
                 -velocity_limit_factor * iiwa14_velocity_limits,
@@ -95,29 +133,38 @@ class PlanarPushingPositionControl:
             )
         )
 
-        self.differential_ik = builder.AddSystem(
+        self.differential_ik = builder.AddNamedSystem(
+            "diff_ik",
             DifferentialInverseKinematicsIntegrator(
                 self.robot,
                 self.robot.GetFrameByName("iiwa_link_7"),
                 time_step,
                 ik_params,
-            )
+            ),
         )
 
         builder.Connect(
             self.differential_ik.GetOutputPort("joint_positions"),
-            self.station.GetInputPort("iiwa_position"),
+            self.hardware_interface.GetInputPort("iiwa_position"),
         )
 
-    def _connect_traj_publisher(self, builder: DiagramBuilder) -> None:
-        # TODO(bernhardpg)
+    def _add_pose_publisher(
+        self, builder: DiagramBuilder, traj: PlanarPushingTrajectory
+    ) -> None:
+        # TODO(bernhardpg): Should not hardcode this
+        PUSHER_HEIGHT = 0.15
+        self.pose_publisher = builder.AddNamedSystem(
+            "pusher_pose_publisher",
+            PusherPosePublisher(traj, PUSHER_HEIGHT),
+        )
         builder.Connect(
-            to_pose.get_output_port(), self.differential_ik.GetInputPort("X_WE_desired")
+            self.pose_publisher.get_output_port(),
+            self.differential_ik.GetInputPort("X_WE_desired"),
         )
 
     def _initialize_ik(self) -> None:
-        q0 = self.station.GetOutputPort("iiwa_position_measured").Eval(
-            self.station_context
+        q0 = self.hardware_interface.GetOutputPort("iiwa_position_measured").Eval(
+            self.hardware_interface_context
         )
         self.differential_ik.get_mutable_parameters().set_nominal_joint_position(q0)
         self.differential_ik.SetPositions(
@@ -129,3 +176,10 @@ class PlanarPushingPositionControl:
 
     def run(self, end_time: float = 1e8) -> None:
         self.simulator.AdvanceTo(end_time)
+
+    def export_diagram(self, filename: str):
+        import pydot
+
+        gviz_string = self.diagram.GetGraphvizString()
+        pydot.graph_from_dot_data(gviz_string)[0].write_png(filename)  # type: ignore
+        print(f"Saved diagram to: {filename}")
