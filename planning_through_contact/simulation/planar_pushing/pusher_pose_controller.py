Fix wrong computation of saturation varibles

planning_through_contact/simulation/planar_pushing/pusher_pose_controller.py
@@ -34,6 +34,10 @@ class PusherPoseController(LeafSystem):
             "pusher_planar_pose_desired",
             AbstractValue.Make(PlanarPose(x=0, y=0, theta=0)),
         )
+        self.pusher_pose_measured = self.DeclareAbstractInputPort(
+            "pusher_pose_measured",
+            AbstractValue.Make(RigidTransform()),
+        )
         self.slider_planar_pose_desired = self.DeclareAbstractInputPort(
             "slider_planar_pose_desired",
             AbstractValue.Make(PlanarPose(x=0, y=0, theta=0)),
@@ -65,6 +69,7 @@ class PusherPoseController(LeafSystem):
         slider: RigidBody,
         contact_mode_desired: OutputPort,
         pusher_planar_pose_desired: OutputPort,
+        pusher_planar_pose_measured: OutputPort,
         slider_planar_pose_desired: OutputPort,
         slider_theta_dot_desired: OutputPort,
         slider_pose_measured: OutputPort,
@@ -77,6 +82,10 @@ class PusherPoseController(LeafSystem):
         builder.Connect(
             contact_mode_desired,
             pusher_pose_controller.GetInputPort("contact_mode_desired"),
+        )
+        builder.Connect(
+            pusher_planar_pose_measured,
+            pusher_pose_controller.GetInputPort("pusher_pose_measured"),
         )
         builder.Connect(
             pusher_planar_pose_desired,
@@ -106,6 +115,8 @@ class PusherPoseController(LeafSystem):
         theta_desired: float,
         theta_dot: float,
         theta_dot_desired: float,
+        p_W_c: npt.NDArray[np.float64],
+        p_WB: npt.NDArray[np.float64],
         loc: PolytopeContactLocation,
     ) -> npt.NDArray[np.float64]:
         theta_error = theta_desired - theta
@@ -117,10 +128,12 @@ class PusherPoseController(LeafSystem):
         K_D = 0.01
         # K_D = 0
 
-        print(f"theta: {theta_error}, theta_dot: {theta_dot_error}")
-
         # Commanded difference in position along contact face
         delta_lam = -(K_P * theta_error + K_D * theta_dot_error)
+
+        print(
+            f"theta: {theta_error}, theta_dot: {theta_dot_error}, delta_lam: {delta_lam}"
+        )
 
         pv1, pv2 = self.object_geometry.get_proximate_vertices_from_location(loc)
 
@@ -128,9 +141,22 @@ class PusherPoseController(LeafSystem):
         dir_vector = (pv2 - pv1) / np.linalg.norm(pv2 - pv1)
         delta_p_B_c = delta_lam * dir_vector
 
-        R_WB = two_d_rotation_matrix_from_angle(theta_desired)
+        R_WB = two_d_rotation_matrix_from_angle(theta)
+        p_B_c = R_WB.T.dot(p_W_c - p_WB)
+
+        p_B_c_cmd = p_B_c + delta_p_B_c
+        cmd_between_pv1_and_pv2 = (
+            np.cross(pv2.flatten(), p_B_c_cmd.flatten()) >= 0
+            and np.cross(pv1.flatten(), p_B_c_cmd.flatten()) <= 0
+        )
+        if not cmd_between_pv1_and_pv2:  # saturate control
+            print("saturate")
+            delta_p_B_c = np.zeros((2, 1))
+
         # We only care about the diffs here, i.e. no constant translation
-        delta_p_W_c = R_WB.dot(delta_p_B_c)
+        R_WB_desired = two_d_rotation_matrix_from_angle(theta_desired)
+        delta_p_W_c = R_WB_desired.dot(delta_p_B_c)
+
         return delta_p_W_c
 
     def DoCalcOutput(self, context: Context, output):
@@ -154,11 +180,16 @@ class PusherPoseController(LeafSystem):
             slider_theta_dot = slider_spatial_velocity.rotational()[Z_AXIS]
             slider_theta_dot_desired: float = self.slider_theta_dot_desired.Eval(context)  # type: ignore
 
+            pusher_pose: RigidTransform = self.pusher_pose_measured.Eval(context)  # type: ignore
+            pusher_planar_pose = PlanarPose.from_pose(pusher_pose)
+
             delta_p_W_c = self._compute_control(
                 slider_planar_pose.theta,
                 slider_planar_pose_desired.theta,
                 slider_theta_dot,
                 slider_theta_dot_desired,
+                pusher_planar_pose.pos(),
+                slider_planar_pose.pos(),
                 mode_desired.to_contact_location(),
             )
             pusher_planar_pose_adjustment = PlanarPose(