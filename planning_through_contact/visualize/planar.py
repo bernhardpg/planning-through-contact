7e77c2d89216772cf6b17ab6942fde90524aa4d3
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2023-09-24 15:08:37 -0400

    Make it possible to visualize start and goal

--- planning_through_contact/visualize/planar.py
+++ planning_through_contact/visualize/planar.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Tuple
 
 import numpy as np
 import numpy.typing as npt
@@ -108,12 +108,191 @@ def _add_pusher_geometry(
             "pusher",
         ),
     )
-    FINGER_COLOR = COLORS["firebrick3"]
+    pusher_COLOR = COLORS["firebrick3"]
     scene_graph.AssignRole(
         source_id,
         pusher_geometry_id,
-        MakePhongIllustrationProperties(FINGER_COLOR.diffuse(alpha)),
+        MakePhongIllustrationProperties(pusher_COLOR.diffuse(alpha)),
     )
+
+
+class PlanarPushingStartGoalGeometry(LeafSystem):
+    """
+    Class used to visualize start and goal positions for a planar pushing task
+    """
+
+    def __init__(
+        self,
+        slider_initial_pose: PlanarPose,
+        slider_target_pose: PlanarPose,
+        pusher_initial_pose: PlanarPose,
+        pusher_target_pose: PlanarPose,
+        slider_geometry: CollisionGeometry,
+        pusher_radius: float,
+        scene_graph: SceneGraph,
+    ) -> None:
+        super().__init__()
+
+        self.slider_initial_pose = slider_initial_pose
+        self.slider_target_pose = slider_target_pose
+        self.pusher_initial_pose = pusher_initial_pose
+        self.pusher_target_pose = pusher_target_pose
+
+        self.DeclareAbstractOutputPort(
+            "geometry_pose",
+            alloc=lambda: Value(FramePoseVector()),
+            calc=self.calc_output,  # type: ignore
+        )
+
+        self.source_id = scene_graph.RegisterSource()
+
+        TRANSPARENCY = 0.3
+
+        self.slider_frame_id = scene_graph.RegisterFrame(
+            self.source_id, GeometryFrame("slider_start")
+        )
+        _add_slider_geometries(
+            self.source_id,
+            slider_geometry,
+            scene_graph,
+            self.slider_frame_id,
+            alpha=TRANSPARENCY,
+        )
+        self.pusher_frame_id = scene_graph.RegisterFrame(
+            self.source_id,
+            GeometryFrame("pusher_start"),
+        )
+        _add_pusher_geometry(
+            self.source_id,
+            pusher_radius,
+            scene_graph,
+            self.pusher_frame_id,
+            alpha=TRANSPARENCY,
+        )
+
+    @classmethod
+    def add_to_builder(
+        cls,
+        builder: DiagramBuilder,
+        slider_initial_pose: PlanarPose,
+        slider_target_pose: PlanarPose,
+        pusher_initial_pose: PlanarPose,
+        pusher_target_pose: PlanarPose,
+        slider_geometry: CollisionGeometry,
+        pusher_radius: float,
+        scene_graph: SceneGraph,
+        name: str = "start_goal_geometry",
+    ) -> "PlanarPushingTrajectory":
+        traj_geometry = builder.AddNamedSystem(
+            name,
+            cls(
+                slider_initial_pose,
+                slider_target_pose,
+                pusher_initial_pose,
+                pusher_target_pose,
+                slider_geometry,
+                pusher_radius,
+                scene_graph,
+            ),
+        )
+        builder.Connect(
+            traj_geometry.get_output_port(),
+            scene_graph.get_source_pose_port(traj_geometry.source_id),
+        )
+        return traj_geometry
+
+    def _set_outputs(
+        self,
+        slider_frame_id: FrameId,
+        pusher_frame_id: FrameId,
+        output: FramePoseVector,
+        p_WB: npt.NDArray[np.float64],
+        p_WP: npt.NDArray[np.float64],
+        R_WB: npt.NDArray[np.float64],
+    ):
+        p_x = p_WB[0, 0]  # type: ignore
+        p_y = p_WB[1, 0]  # type: ignore
+
+        slider_pose = RigidTransform(
+            RotationMatrix(R_WB), np.array([p_x, p_y, 0.0])  # type: ignore
+        )
+        output.get_mutable_value().set_value(id=slider_frame_id, value=slider_pose)  # type: ignore
+
+        pusher_pose = RigidTransform(
+            RotationMatrix.Identity(), np.concatenate((p_WP.flatten(), [0]))  # type: ignore
+        )
+        output.get_mutable_value().set_value(id=pusher_frame_id, value=pusher_pose)  # type: ignore
+
+    def _get_pusher_in_world(self, slider_pose, pusher_pose) -> PlanarPose:
+        p_WP = slider_pose.pos() + slider_pose.two_d_rot_matrix().dot(pusher_pose.pos())
+        return PlanarPose(p_WP[0, 0], p_WP[1, 0], 0)
+
+    def get_pos_limits(
+        self, slider_geometry, buffer
+    ) -> Tuple[float, float, float, float]:
+        def get_lims(vecs) -> Tuple[float, float, float, float]:
+            vec_xs = [vec[0, 0] for vec in vecs]
+            vec_ys = [vec[1, 0] for vec in vecs]
+
+            vec_x_max = max(vec_xs)
+            vec_x_min = min(vec_xs)
+            vec_y_max = max(vec_ys)
+            vec_y_min = min(vec_ys)
+
+            return vec_x_min, vec_x_max, vec_y_min, vec_y_max
+
+        def add_buffer_to_lims(lims, buffer) -> Tuple[float, float, float, float]:
+            return (
+                lims[0] - buffer,
+                lims[1] + buffer,
+                lims[2] - buffer,
+                lims[3] + buffer,
+            )
+
+        def get_lims_from_two_lims(lim_a, lim_b) -> Tuple[float, float, float, float]:
+            return (
+                min(lim_a[0], lim_b[0]),
+                max(lim_a[1], lim_b[1]),
+                min(lim_a[2], lim_b[2]),
+                max(lim_a[3], lim_b[3]),
+            )
+
+        p_WB_lims = get_lims(
+            [self.slider_initial_pose.pos(), self.slider_target_pose.pos()]
+        )
+        object_radius = slider_geometry.max_dist_from_com
+        obj_lims = add_buffer_to_lims(p_WB_lims, object_radius)
+        p_WP_lims = get_lims(
+            [
+                self._get_pusher_in_world(
+                    self.slider_initial_pose, self.pusher_initial_pose
+                ).pos(),
+                self._get_pusher_in_world(
+                    self.slider_target_pose, self.pusher_target_pose
+                ).pos(),
+            ]
+        )
+
+        lims = get_lims_from_two_lims(obj_lims, p_WP_lims)
+        return add_buffer_to_lims(lims, buffer)
+
+    def calc_output(self, context: Context, output: FramePoseVector) -> None:
+        t = context.get_time()
+        if t <= 1.0:
+            slider_pose = self.slider_initial_pose
+            pusher_pose = self.pusher_initial_pose
+        else:
+            slider_pose = self.slider_target_pose
+            pusher_pose = self.pusher_target_pose
+
+        self._set_outputs(
+            self.slider_frame_id,
+            self.pusher_frame_id,
+            output,
+            slider_pose.pos(),
+            self._get_pusher_in_world(slider_pose, pusher_pose).pos(),
+            slider_pose.rot_matrix(),
+        )
 
 
 class PlanarPushingTrajectoryGeometry(LeafSystem):
@@ -281,6 +460,77 @@ class PlanarPushingTrajectoryGeometry(LeafSystem):
             )
 
 
+def visualize_planar_pushing_start_and_goal(
+    slider_geometry: CollisionGeometry,
+    pusher_radius: float,
+    slider_initial_pose: PlanarPose,
+    slider_target_pose: PlanarPose,
+    pusher_initial_pose: PlanarPose,
+    pusher_target_pose: PlanarPose,
+    show: bool = False,
+    save: bool = False,
+    filename: Optional[str] = None,
+):
+    if save:
+        assert filename is not None
+
+    builder = DiagramBuilder()
+
+    # Register geometry with SceneGraph
+    scene_graph = builder.AddNamedSystem("scene_graph", SceneGraph())
+    geometry = PlanarPushingStartGoalGeometry.add_to_builder(
+        builder,
+        slider_initial_pose,
+        slider_target_pose,
+        pusher_initial_pose,
+        pusher_target_pose,
+        slider_geometry,
+        pusher_radius,
+        scene_graph,
+    )
+
+    x_min, x_max, y_min, y_max = geometry.get_pos_limits(slider_geometry, buffer=0.1)
+
+    def connect_planar_visualizer(
+        builder: DiagramBuilder, scene_graph: SceneGraph
+    ) -> PlanarSceneGraphVisualizer:
+        T_VW = np.array(
+            [[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0]]
+        )
+        visualizer = ConnectPlanarSceneGraphVisualizer(
+            builder,
+            scene_graph,
+            T_VW=T_VW,
+            xlim=np.array([x_min, x_max]),
+            ylim=np.array([y_min, y_max]),
+            show=show,
+        )
+        return visualizer
+
+    visualizer = connect_planar_visualizer(builder, scene_graph)
+
+    diagram = builder.Build()
+    diagram.set_name("diagram")
+
+    # Create the simulator, and simulate for 10 seconds.
+    context = diagram.CreateDefaultContext()
+    simulator = Simulator(diagram, context)
+
+    visualizer.start_recording()  # type: ignore
+
+    simulator.Initialize()
+    simulator.AdvanceTo(2.0)
+
+    visualizer.stop_recording()  # type: ignore
+    ani = visualizer.get_recording_as_animation()  # type: ignore
+
+    if save:
+        # Playback the recording and save the output.
+        ani.save(f"{filename}.mp4", fps=30)
+
+    return ani
+
+
 def visualize_planar_pushing_trajectory(
     traj: PlanarPushingTrajectory,
     show: bool = False,
@@ -354,7 +604,7 @@ def visualize_planar_pushing_trajectory_legacy(
 ) -> None:
     CONTACT_COLOR = COLORS["dodgerblue4"]
     BOX_COLOR = COLORS["aquamarine4"]
-    FINGER_COLOR = COLORS["firebrick3"]
+    pusher_COLOR = COLORS["firebrick3"]
     TARGET_COLOR = COLORS["firebrick1"]
 
     flattened_rotation = np.vstack([R.flatten() for R in traj.R_WB])
@@ -373,7 +623,7 @@ def visualize_planar_pushing_trajectory_legacy(
         TARGET_COLOR,
     )
 
-    contact_point_viz = VisualizationPoint2d(traj.p_WP.T, FINGER_COLOR)
+    contact_point_viz = VisualizationPoint2d(traj.p_WP.T, pusher_COLOR)
     contact_point_viz.change_radius(pusher_radius)
 
     contact_force_viz = VisualizationForce2d(traj.p_WP.T, CONTACT_COLOR, traj.f_c_W.T)
