410d9557084a4f20c067143374a8e137352af9aa
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2023-11-19 21:28:02 -0500

    Minimize squared angular velocities

--- planning_through_contact/experiments/rotation_tightness/angular_velocity.py
+++ planning_through_contact/experiments/rotation_tightness/angular_velocity.py
@@ -25,18 +25,23 @@ for i in range(NUM_CTRL_POINTS):
     so_2_constraint = r_i.T.dot(r_i) == 1
     prog.AddConstraint(so_2_constraint)
 
+USE_SQ_EUCL_DISTANCE = False
 # Minimize squared euclidean distances in rotation parameters
+r_displacements = []
 for i in range(NUM_CTRL_POINTS - 1):
     r_i = r[:, i]
     r_next = r[:, i + 1]
-    r_dot_i = r_next - r_i
+    r_disp_i = r_next - r_i
+    r_displacements.append(r_disp_i)
 
-    rot_cost_i = r_dot_i.T.dot(r_dot_i)
-    prog.AddCost(rot_cost_i)
+    rot_cost_i = r_disp_i.T.dot(r_disp_i)
+
+    if USE_SQ_EUCL_DISTANCE:
+        prog.AddCost(rot_cost_i)
 
 # Initial conditions
-th_initial = np.pi
-th_final = 0 + 0.1
+th_initial = 0
+th_final = np.pi + 0.1
 
 create_r_vec_from_angle = lambda th: np.array([np.cos(th), np.sin(th)])
 
@@ -85,36 +90,38 @@ for k in range(NUM_CTRL_POINTS - 1):
         [convert_formula_to_lhs_expression(f) for f in constraint.flatten()]
     )
 
-# prog.AddCost(np.sum(th_dots))
-
+prog.AddCost(th_dots.T @ th_dots)
 
 # Solve SDP relaxation
-# relaxed_prog, X, mon_basis = create_sdp_relaxation(prog)
 relaxed_prog = MakeSemidefiniteRelaxation(prog)
+
+# Is not tight
+USE_EUCLIDEAN_DISTANCE = True
+if USE_EUCLIDEAN_DISTANCE:
+    s = relaxed_prog.NewContinuousVariables(NUM_CTRL_POINTS - 1, "s")
+    relaxed_prog.AddCost(np.sum(s))
+
+    for k in range(NUM_CTRL_POINTS - 1):
+        mat = np.zeros((3, 3), dtype=object)
+        mat[:2, :2] = np.eye(2) * s[k]
+        mat[2, :2] = r_displacements[k].T
+        mat[:2, 2] = r_displacements[k]
+        mat[2, 2] = s[k]
+
+        relaxed_prog.AddPositiveSemidefiniteConstraint(mat)
+
+
 print("Finished formulating SDP relaxation")
 
-# solver_options = SolverOptions()
-# solver_options.SetOption(CommonSolverOption.kPrintToConsole, 1)  # type: ignore
+solver_options = SolverOptions()
+solver_options.SetOption(CommonSolverOption.kPrintToConsole, 1)  # type: ignore
 
-result = Solve(relaxed_prog)
+result = Solve(relaxed_prog, solver_options=solver_options)
 assert result.is_success()
 print(f"Cost: {result.get_optimal_cost()}")
 
-# X_val = result.GetSolution(X)
-#
-# tol = 1e-4
-# num_nonzero_eigvals = len(
-#     [val for val in np.linalg.eigvals(X_val) if np.abs(val) >= tol]
-# )
-# print(f"Rank of X: {num_nonzero_eigvals}")
-# print(f"cost: {result.get_optimal_cost()}")
-#
-# x_val = X_val[:, 0]
-
 r_val = result.GetSolution(r)
 r_val = r_val.reshape((NUM_DIMS, NUM_CTRL_POINTS), order="F")
-
-breakpoint()
 
 
 plot_cos_sine_trajs(r_val.T)
