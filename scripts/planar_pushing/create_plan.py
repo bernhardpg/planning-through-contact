b51999099a04f74beb8f88730d8b96360f9a86ab
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2023-12-07 10:32:52 -0500

    Clean up create plan script

--- scripts/planar_pushing/create_plan.py
+++ scripts/planar_pushing/create_plan.py
@@ -44,64 +44,7 @@ def get_sugar_box() -> RigidBody:
     return slider
 
 
-def create_plan(
-    debug: bool = False,
-    body_to_use: Literal["box", "t_pusher", "sugar_box"] = "sugar_box",
-    traj_number: int = 1,
-    visualize: bool = False,
-    save_traj: bool = False,
-):
-    if body_to_use == "box":
-        slider = get_slider_box()
-    elif body_to_use == "t_pusher":
-        slider = get_tee()
-    elif body_to_use == "sugar_box":
-        slider = get_sugar_box()
-
-    pusher_radius = 0.035
-    dynamics_config = SliderPusherSystemConfig(
-        pusher_radius=pusher_radius, slider=slider, friction_coeff_slider_pusher=0.25
-    )
-
-    cost_terms = PlanarCostFunctionTerms(
-        sq_forces=10.0,
-        ang_displacements=1.0,
-        lin_displacements=1.0,
-        obj_avoidance_quad_weight=0.4,
-        mode_transition_cost=1.0,
-    )
-
-    config = PlanarPlanConfig(
-        dynamics_config=dynamics_config,
-        cost_terms=cost_terms,
-        time_in_contact=7,
-        time_non_collision=7,
-        num_knot_points_contact=3,
-        num_knot_points_non_collision=3,
-        avoid_object=True,
-        avoidance_cost="quadratic",
-        allow_teleportation=False,
-        use_band_sparsity=True,
-        minimize_sq_forces=True,
-        use_entry_and_exit_subgraphs=True,
-        penalize_mode_transitions=False,
-        minimize_keypoint_displacement=True,
-    )
-
-    planner = PlanarPushingPlanner(config)
-
-    solver_params = PlanarSolverParams(
-        measure_solve_time=True,
-        gcs_max_rounded_paths=20,
-        print_flows=False,
-        print_solver_output=True,
-        save_solver_output=False,
-        print_path=True,
-        print_cost=True,
-        nonlinear_traj_rounding=False,
-        assert_result=False,
-    )
-
+def get_predefined_plan(traj_number: int) -> PlanarPushingStartAndGoal:
     if traj_number == 1:
         slider_initial_pose = PlanarPose(x=0.55, y=0.0, theta=0.0)
         slider_target_pose = PlanarPose(x=0.65, y=0.0, theta=-0.5)
@@ -171,8 +114,74 @@ def create_plan(
     else:
         raise NotImplementedError()
 
-    planner.set_initial_poses(pusher_initial_pose, slider_initial_pose)
-    planner.set_target_poses(pusher_target_pose, slider_target_pose)
+    return PlanarPushingStartAndGoal(
+        slider_initial_pose, slider_target_pose, pusher_initial_pose, pusher_target_pose
+    )
+
+
+def create_plan(
+    plan_spec: PlanarPushingStartAndGoal,
+    body_to_use: Literal["box", "t_pusher", "sugar_box"] = "sugar_box",
+    traj_name: str = "Untitled_traj",
+    visualize: bool = False,
+    save_traj: bool = False,
+    debug: bool = False,
+):
+    if body_to_use == "box":
+        slider = get_slider_box()
+    elif body_to_use == "t_pusher":
+        slider = get_tee()
+    elif body_to_use == "sugar_box":
+        slider = get_sugar_box()
+
+    pusher_radius = 0.035
+    dynamics_config = SliderPusherSystemConfig(
+        pusher_radius=pusher_radius, slider=slider, friction_coeff_slider_pusher=0.25
+    )
+
+    cost_terms = PlanarCostFunctionTerms(
+        sq_forces=10.0,
+        ang_displacements=1.0,
+        lin_displacements=1.0,
+        obj_avoidance_quad_weight=0.4,
+        mode_transition_cost=1.0,
+    )
+
+    config = PlanarPlanConfig(
+        dynamics_config=dynamics_config,
+        cost_terms=cost_terms,
+        time_in_contact=7,
+        time_non_collision=7,
+        num_knot_points_contact=3,
+        num_knot_points_non_collision=3,
+        avoid_object=True,
+        avoidance_cost="quadratic",
+        allow_teleportation=False,
+        use_band_sparsity=True,
+        minimize_sq_forces=True,
+        use_entry_and_exit_subgraphs=True,
+        penalize_mode_transitions=False,
+        minimize_keypoint_displacement=True,
+    )
+
+    planner = PlanarPushingPlanner(config)
+
+    solver_params = PlanarSolverParams(
+        measure_solve_time=True,
+        gcs_max_rounded_paths=20,
+        print_flows=False,
+        print_solver_output=True,
+        save_solver_output=False,
+        print_path=True,
+        print_cost=True,
+        nonlinear_traj_rounding=False,
+        assert_result=False,
+    )
+
+    planner.set_initial_poses(
+        plan_spec.pusher_initial_pose, plan_spec.slider_initial_pose
+    )
+    planner.set_target_poses(plan_spec.pusher_target_pose, plan_spec.slider_target_pose)
     planner.formulate_problem()
 
     if debug:
@@ -181,28 +190,22 @@ def create_plan(
     traj = planner.plan_trajectory(solver_params)
 
     if save_traj:
-        traj_name = f"trajectories/{body_to_use}_pushing_{traj_number}.pkl"
-        traj.save(traj_name)  # type: ignore
+        filename = f"trajectories/{body_to_use}_pushing_{traj_name}.pkl"
+        traj.save(filename)  # type: ignore
 
     if visualize:
-        plan = PlanarPushingStartAndGoal(
-            slider_initial_pose,
-            slider_target_pose,
-            pusher_initial_pose,
-            pusher_target_pose,
-        )
         visualize_planar_pushing_start_and_goal(
             config.slider_geometry,
             config.pusher_radius,
-            plan,
+            plan_spec,
             save=True,
-            filename=f"trajectory_{traj_number}_start_and_goal_{body_to_use}",
+            filename=f"trajectory_{traj_name}_start_and_goal_{body_to_use}",
         )
 
         ani = visualize_planar_pushing_trajectory(
             traj,  # type: ignore
             save=True,
-            filename=f"trajectory_{traj_number}_{body_to_use}",
+            filename=f"trajectory_{traj_name}_{body_to_use}",
             visualize_knot_points=True,
         )
         return ani
@@ -224,10 +227,14 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     traj_number = args.traj
+
+    plan_spec = get_predefined_plan(traj_number)
+
     create_plan(
+        plan_spec,
         debug=True,
         body_to_use=args.body,
-        traj_number=traj_number,
+        traj_name=str(traj_number),
         visualize=True,
         save_traj=True,
     )
