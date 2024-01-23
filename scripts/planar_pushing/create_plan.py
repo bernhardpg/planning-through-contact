08f8eb42164a44867fc112e5b7b60d94d4f85f3e
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2024-01-23 18:00:20 +0100

    Make logic to throw away plans with collision

--- scripts/planar_pushing/create_plan.py
+++ scripts/planar_pushing/create_plan.py
@@ -13,9 +13,14 @@ from planning_through_contact.experiments.utils import (
 from planning_through_contact.geometry.collision_geometry.box_2d import Box2d
 from planning_through_contact.geometry.collision_geometry.collision_geometry import (
     CollisionGeometry,
+    ContactLocation,
+    PolytopeContactLocation,
 )
 from planning_through_contact.geometry.collision_geometry.t_pusher_2d import TPusher2d
 from planning_through_contact.geometry.planar.face_contact import FaceContactMode
+from planning_through_contact.geometry.planar.non_collision import (
+    check_finger_pose_in_contact_location,
+)
 from planning_through_contact.geometry.planar.planar_pose import PlanarPose
 from planning_through_contact.geometry.rigid_body import RigidBody
 from planning_through_contact.planning.planar.planar_plan_config import (
@@ -154,14 +159,47 @@ def _slider_within_workspace(
     return vertices_within_workspace
 
 
+# TODO: refactor
+def _check_collision(
+    pusher_pose_world: PlanarPose,
+    slider_pose_world: PlanarPose,
+    config: PlanarPlanConfig,
+) -> bool:
+    p_WP = pusher_pose_world.pos()
+    R_WB = slider_pose_world.two_d_rot_matrix()
+    p_WB = slider_pose_world.pos()
+
+    # We need to compute the pusher pos in the frame of the slider
+    p_BP = R_WB.T @ (p_WP - p_WB)
+    pusher_pose_body = PlanarPose(p_BP[0, 0], p_BP[1, 0], 0)
+
+    # we always add all non-collision modes, even when we don't add all contact modes
+    # (think of maneuvering around the object etc)
+    locations = [
+        PolytopeContactLocation(ContactLocation.FACE, idx)
+        for idx in range(config.slider_geometry.num_collision_free_regions)
+    ]
+    matching_locs = [
+        loc
+        for loc in locations
+        if check_finger_pose_in_contact_location(pusher_pose_body, loc, config)
+    ]
+    if len(matching_locs) == 0:
+        return True
+    else:
+        return False
+
+
 def _get_slider_pose_within_workspace(
     workspace: PlanarPushingWorkspace,
     slider: CollisionGeometry,
+    pusher_pose: PlanarPose,
+    config: PlanarPlanConfig,
     limit_rotations: bool = False,
 ) -> PlanarPose:
     valid_pose = False
 
-    pose = None
+    slider_pose = None
     while not valid_pose:
         x_initial = np.random.uniform(workspace.slider.x_min, workspace.slider.x_max)
         y_initial = np.random.uniform(workspace.slider.y_min, workspace.slider.y_max)
@@ -171,33 +209,29 @@ def _get_slider_pose_within_workspace(
         else:
             th_initial = np.random.uniform(-np.pi + EPS, np.pi - EPS)
 
-        pose = PlanarPose(x_initial, y_initial, th_initial)
+        slider_pose = PlanarPose(x_initial, y_initial, th_initial)
 
-        valid_pose = _slider_within_workspace(workspace, pose, slider)
+        collides_with_pusher = _check_collision(pusher_pose, slider_pose, config)
+        within_workspace = _slider_within_workspace(workspace, slider_pose, slider)
 
-    assert pose is not None  # fix LSP errors
+        valid_pose = within_workspace and not collides_with_pusher
 
-    return pose
+    assert slider_pose is not None  # fix LSP errors
+
+    return slider_pose
 
 
 def get_plans_to_point(
     num_plans: int,
-    slider_type: Literal["box", "sugar_box", "tee"],
-    lims: Tuple[float, float, float, float],
     workspace: PlanarPushingWorkspace,
-    pusher_radius: float,
+    config: PlanarPlanConfig,
     point: Tuple[float, float] = (0, 0),  # Default is origin
     limit_rotations: bool = True,  # Use this to start with
 ) -> List[PlanarPushingStartAndGoal]:
     # We want the plans to always be the same
     np.random.seed(1)
 
-    if slider_type == "box":
-        slider = get_box()
-    elif slider_type == "tee":
-        slider = get_tee()
-    elif slider_type == "sugar_box":
-        slider = get_sugar_box()
+    slider = config.slider_geometry
 
     # Hardcoded pusher start pose
     pusher_pose = PlanarPose(0.61, 0.16, 0)
@@ -205,7 +239,7 @@ def get_plans_to_point(
     plans = []
     for _ in range(num_plans):
         slider_initial_pose = _get_slider_pose_within_workspace(
-            workspace, slider.geometry, limit_rotations
+            workspace, slider, pusher_pose, config, limit_rotations
         )
 
         slider_target_pose = PlanarPose(point[0], point[1], 0)
@@ -225,95 +259,16 @@ def create_plan(
     traj_name: str = "Untitled_traj",
     visualize: bool = False,
     pusher_radius: float = 0.035,
-    time_in_contact: float = 7,
     do_rounding: bool = True,
-    time_in_non_collision: float = 7,
     animation_output_dir: str = "",
     interpolate_video: bool = False,
     animation_lims: Optional[Tuple[float, float, float, float]] = None,
     save_traj: bool = False,
     save_analysis: bool = False,
     debug: bool = False,
-    use_old_params: bool = False,
 ):
     if animation_output_dir != "":
         traj_name = animation_output_dir + "/" + traj_name
-
-    if use_old_params:
-        if slider_type == "box":
-            slider = get_box()
-        elif slider_type == "tee":
-            slider = get_tee()
-        elif slider_type == "sugar_box":
-            slider = get_sugar_box()
-
-        dynamics_config = SliderPusherSystemConfig(
-            pusher_radius=pusher_radius,
-            slider=slider,
-            friction_coeff_slider_pusher=0.25,
-            friction_coeff_table_slider=0.5,
-            integration_constant=0.02,
-        )
-
-        # Configure contact cost
-        contact_cost = ContactCost(
-            cost_type=ContactCostType.OPTIMAL_CONTROL,
-            force_regularization=5.0,
-            ang_displacements=1.0,
-            lin_displacements=1.0,
-            mode_transition_cost=None,
-        )
-
-        contact_config = ContactConfig(
-            cost=contact_cost,
-            lam_min=0.47,
-            lam_max=0.53,
-            delta_vel_max=0.05 * 2,
-            delta_theta_max=0.4 * 2,
-        )
-
-        # Configure non-collision cost
-        non_collision_cost = NonCollisionCost(
-            distance_to_object_quadratic=None,
-            distance_to_object_socp=1.0,
-            pusher_velocity_regularization=1.0,
-            pusher_arc_length=None,
-        )
-
-        config = PlanarPlanConfig(
-            dynamics_config=dynamics_config,
-            time_in_contact=time_in_contact,
-            time_non_collision=time_in_non_collision,
-            num_knot_points_contact=4,
-            num_knot_points_non_collision=4,
-            allow_teleportation=False,
-            use_band_sparsity=True,
-            use_entry_and_exit_subgraphs=True,
-            contact_config=contact_config,
-            continuity_on_pusher_velocity=True,
-            non_collision_cost=non_collision_cost,
-        )
-
-        solver_params = PlanarSolverParams(
-            measure_solve_time=True,
-            gcs_max_rounded_paths=20,
-            print_flows=False,
-            print_solver_output=debug,
-            save_solver_output=False,
-            print_path=debug,
-            print_cost=debug,
-            assert_result=True,
-        )
-
-    else:
-        config = get_default_plan_config(
-            slider_type=slider_type,
-            pusher_radius=pusher_radius,
-            integration_constant=0.3,
-            friction_coeff=0.05,
-            lam_buffer=0.4,
-        )
-        solver_params = get_default_solver_params(debug, clarabel=False)
 
     if debug:
         visualize_planar_pushing_start_and_goal(
@@ -445,50 +400,93 @@ if __name__ == "__main__":
     debug = args.debug
     rounding = args.round
     interpolate = args.interpolate
+    slider_type = args.body
 
     pusher_radius = 0.015
 
-    if make_demos:
-        lims = (-0.4, 0.4, -0.4, 0.4)
-        animation_lims = (np.array(lims) * 1.3).tolist()
-        plans = get_plans_to_point(9, lims, pusher_radius)
+    time_in_contact = 6.0
+    time_in_non_collision = 2.0
 
-        if traj_number is not None:
-            create_plan(
-                plans[traj_number],
-                debug=debug,
-                slider_type=args.body,
-                traj_name=f"demo_{traj_number}",
-                visualize=True,
-                pusher_radius=pusher_radius,
-                save_traj=True,
-                animation_output_dir="demos",
-                animation_lims=animation_lims,
-                time_in_contact=2.0,
-                time_in_non_collision=1.0,
-                interpolate_video=interpolate,
-                save_analysis=True,
-                do_rounding=rounding,
-            )
+    use_old_params = False
+    if use_old_params:
+        if slider_type == "box":
+            slider = get_box()
+        elif slider_type == "tee":
+            slider = get_tee()
+        elif slider_type == "sugar_box":
+            slider = get_sugar_box()
         else:
-            for idx, plan in enumerate(plans):
-                create_plan(
-                    plan,
-                    debug=debug,
-                    slider_type=args.body,
-                    traj_name=f"demo_{idx}",
-                    visualize=True,
-                    pusher_radius=pusher_radius,
-                    save_traj=True,
-                    animation_output_dir="demos",
-                    animation_lims=animation_lims,
-                    time_in_contact=2.0,
-                    time_in_non_collision=1.0,
-                    interpolate_video=interpolate,
-                    save_analysis=True,
-                    do_rounding=rounding,
-                )
-    elif hardware_demos:
+            raise NotImplementedError("")
+
+        dynamics_config = SliderPusherSystemConfig(
+            pusher_radius=pusher_radius,
+            slider=slider,
+            friction_coeff_slider_pusher=0.25,
+            friction_coeff_table_slider=0.5,
+            integration_constant=0.02,
+        )
+
+        # Configure contact cost
+        contact_cost = ContactCost(
+            cost_type=ContactCostType.OPTIMAL_CONTROL,
+            force_regularization=5.0,
+            ang_displacements=1.0,
+            lin_displacements=1.0,
+            mode_transition_cost=None,
+        )
+
+        contact_config = ContactConfig(
+            cost=contact_cost,
+            lam_min=0.47,
+            lam_max=0.53,
+            delta_vel_max=0.05 * 2,
+            delta_theta_max=0.4 * 2,
+        )
+
+        # Configure non-collision cost
+        non_collision_cost = NonCollisionCost(
+            distance_to_object_quadratic=None,
+            distance_to_object_socp=1.0,
+            pusher_velocity_regularization=1.0,
+            pusher_arc_length=None,
+        )
+
+        config = PlanarPlanConfig(
+            dynamics_config=dynamics_config,
+            time_in_contact=time_in_contact,
+            time_non_collision=time_in_non_collision,
+            num_knot_points_contact=4,
+            num_knot_points_non_collision=4,
+            allow_teleportation=False,
+            use_band_sparsity=True,
+            use_entry_and_exit_subgraphs=True,
+            contact_config=contact_config,
+            continuity_on_pusher_velocity=True,
+            non_collision_cost=non_collision_cost,
+        )
+
+        solver_params = PlanarSolverParams(
+            measure_solve_time=True,
+            gcs_max_rounded_paths=20,
+            print_flows=False,
+            print_solver_output=debug,
+            save_solver_output=False,
+            print_path=debug,
+            print_cost=debug,
+            assert_result=True,
+        )
+
+    else:
+        config = get_default_plan_config(
+            slider_type=slider_type,
+            pusher_radius=pusher_radius,
+            integration_constant=0.3,
+            friction_coeff=0.05,
+            lam_buffer=0.4,
+        )
+        solver_params = get_default_solver_params(debug, clarabel=False)
+
+    if hardware_demos:
         workspace = PlanarPushingWorkspace(
             slider=BoxWorkspace(
                 width=0.35,
@@ -498,11 +496,7 @@ if __name__ == "__main__":
             ),
         )
 
-        lims = (0.5, 0.6, -0.15, 0.15)
-        animation_lims = (np.array(lims) * 1.3).tolist()
-        plans = get_plans_to_point(
-            10, args.body, lims, workspace, pusher_radius, (0.575, -0.04285714)
-        )
+        plans = get_plans_to_point(10, workspace, config, (0.575, -0.04285714))
         if traj_number is not None:
             create_plan(
                 plans[traj_number],
@@ -514,8 +508,6 @@ if __name__ == "__main__":
                 save_traj=True,
                 animation_output_dir="demos",
                 animation_lims=None,
-                time_in_contact=6.0,
-                time_in_non_collision=2.0,
                 interpolate_video=interpolate,
                 save_analysis=True,
                 do_rounding=rounding,
@@ -532,8 +524,6 @@ if __name__ == "__main__":
                     save_traj=True,
                     animation_output_dir="demos",
                     animation_lims=None,
-                    time_in_contact=4.0,
-                    time_in_non_collision=2.0,
                     interpolate_video=interpolate,
                     save_analysis=True,
                     do_rounding=rounding,
