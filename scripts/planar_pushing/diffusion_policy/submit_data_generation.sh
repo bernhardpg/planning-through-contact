cb9a0035c6f42a4fe6aa7b3756c18ae9cd15fa75
Adam Wei <weiadam@mit.edu>
2024-04-10 15:05:13 -0400

    initial add: supercloud script

---
+++ scripts/planar_pushing/diffusion_policy/submit_data_generation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Parse command line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -c|--config-name) CONFIG_NAME="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Check if CONFIG_NAME is set
+if [ -z "$CONFIG_NAME" ]; then
+    echo "Configuration name is not provided. Use -c or --config-name to specify the configuration name."
+    exit 1
+fi
+
+# Initialize and Load Modules
+cd ../../../. # Go to the root directory of the project
+source /etc/profile
+poetry shell
+export PYTHONPATH=~/workspace/drake-build-bernhard/install/lib/python3.10/site-packages:~/workspace/gcs-diffusion:
+
+echo $LLSUB_RANK
+echo $LLSUB_SIZE
+
+# Run Python script with provided configuration name
+python scripts/planar_pushing/diffusion_policy/data_generation.py --config-name "$CONFIG_NAME" \
+    data_collection_config.LLSUB_RANK=$LLSUB_RANK \
+    data_collection_config.LLSUB_SIZE=$LLSUB_SIZE
