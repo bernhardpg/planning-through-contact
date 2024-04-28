d88b68d46b7e2d8d0df31ffaaac6819457df0a7d
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2024-04-27 20:42:01 -0400

    Write code for terrain generation

---
+++ planning_through_contact/experiments/unfinished_research/footstep_planning/2d_footstep_planning.py
@@ -0,0 +1,79 @@
+from dataclasses import dataclass
+from typing import Optional
+
+import matplotlib.pyplot as plt
+import numpy as np
+from underactuated.exercises.humanoids.footstep_planning_gcs_utils import plot_rectangle
+
+
+@dataclass
+class InPlaneSteppingStone:
+    x_pos: float
+    z_pos: float
+    width: float
+    name: Optional[str] = None
+
+    @property
+    def height(self) -> float:
+        return self.z_pos
+
+    def plot(self, **kwargs):
+        center = np.array([self.x_pos, self.z_pos / 2])
+
+        return plot_rectangle(center, self.width, self.height, **kwargs)
+
+
+class InPlaneTerrain:
+    def __init__(self) -> None:
+        self.stepping_stones = []
+
+    def add_stone(
+        self,
+        x_pos: float,
+        z_pos: float,
+        width: float,
+        name: Optional[str] = None,
+    ) -> InPlaneSteppingStone:
+        stone = InPlaneSteppingStone(x_pos, z_pos, width)
+        self.stepping_stones.append(stone)
+        return stone
+
+    def get_stone_by_name(self, name):
+        for stone in self.stepping_stones:
+            if stone.name == name:
+                return stone
+
+        raise ValueError(f"No stone in the terrain has name {name}.")
+
+    @property
+    def max_height(self) -> float:
+        return max([s.height for s in self.stepping_stones])
+
+    def plot(self, title: Optional[str] = None, **kwargs):
+        # make light green the default facecolor
+        if not "facecolor" in kwargs:
+            kwargs["facecolor"] = [0, 1, 0, 0.1]
+
+        # plot stepping stones disposition
+        labels = ["Stepping stone", None]
+        for i, stone in enumerate(self.stepping_stones):
+            stone.plot(label=labels[min(i, 1)], **kwargs)
+
+        if title is not None:
+            plt.title(title)
+            # get current plot axis if one is not given
+        ax = plt.gca()
+        ax.set_ylim((0.0, self.max_height * 2.0))
+
+
+def main():
+    terrain = InPlaneTerrain()
+    terrain.add_stone(x_pos=0.5, width=1.0, z_pos=0.2)
+    terrain.add_stone(x_pos=1.5, width=1.0, z_pos=0.3)
+
+    terrain.plot()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
