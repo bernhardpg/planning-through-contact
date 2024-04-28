712a51b05a8a5d734b3b2e37017e277991aa9cbf
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2024-04-27 21:56:36 -0400

    Wrap knot point functionality in a class

--- planning_through_contact/experiments/unfinished_research/footstep_planning/2d_footstep_planning.py
+++ planning_through_contact/experiments/unfinished_research/footstep_planning/2d_footstep_planning.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.typing as npt
 from pydrake.geometry.optimization import (
     GraphOfConvexSets,
     HPolyhedron,
@@ -19,6 +20,7 @@ from pydrake.solvers import (
 from underactuated.exercises.humanoids.footstep_planning_gcs_utils import plot_rectangle
 
 from planning_through_contact.geometry.utilities import cross_2d
+from planning_through_contact.tools.types import NpVariableArray
 
 
 @dataclass
@@ -83,50 +85,79 @@ class InPlaneTerrain:
 
 @dataclass
 class Robot:
+    # TODO(bernhardpg): set reasonable inertia properties
+    # This is from a sphere with mass = 50 kg and all axis = 0.5 meter
     mass: float = 50.0  # kg
     inertia: float = 5.0  # kg m**2
     foot_length: float = 0.3  # m
+    step_span: float = 0.8  # m
 
 
-def create_set(stone: InPlaneSteppingStone, robot: Robot) -> Spectrahedron:
-    # Assume we always only have one foot in contact
-    prog = MathematicalProgram()
+@dataclass
+class KnotPoint:
+    def __init__(
+        self, stone: InPlaneSteppingStone, robot: Robot, name: Optional[str] = None
+    ) -> None:
+        # Assume we always only have one foot in contact
+        if name is not None:
+            self.name = f"{stone.name}_{name}"
 
-    # declare states
-    p_WB = prog.NewContinuousVariables(2, "p_WB")
-    v_WB = prog.NewContinuousVariables(2, "v_WB")
-    theta_WB = prog.NewContinuousVariables(1, "theta_WB")[0]
-    omega_WB = prog.NewContinuousVariables(1, "omega_WB")[0]
+        self.prog = MathematicalProgram()
 
-    # declare inputs
-    p_BF_W = prog.NewContinuousVariables(2, "p_BF_W")
-    f_F_1W = prog.NewContinuousVariables(2, "f_F_1W")
-    f_F_2W = prog.NewContinuousVariables(2, "f_F_2W")
+        # declare states
+        self.p_WB = self.prog.NewContinuousVariables(2, "p_WB")
+        self.v_WB = self.prog.NewContinuousVariables(2, "v_WB")
+        self.theta_WB = self.prog.NewContinuousVariables(1, "theta_WB")[0]
+        self.omega_WB = self.prog.NewContinuousVariables(1, "omega_WB")[0]
 
-    # auxilliary vars
-    # TODO(bernhardpg): we might be able to get around this once we
-    # have SDP constraints over the edges
-    tau_F_1 = prog.NewContinuousVariables(1, "tau_F_1")[0]
-    tau_F_2 = prog.NewContinuousVariables(1, "tau_F_2")[0]
+        # declare inputs
+        self.p_BF_W = self.prog.NewContinuousVariables(2, "p_BF_W")
+        self.f_F_1W = self.prog.NewContinuousVariables(2, "f_F_1W")
+        self.f_F_2W = self.prog.NewContinuousVariables(2, "f_F_2W")
 
-    # linear acceleration
-    x_ddot = (1 / robot.mass) * (f_F_1W[0] + f_F_2W[0])
-    z_ddot = (1 / robot.mass) * (f_F_1W[1] + f_F_2W[1])
+        # auxilliary vars
+        # TODO(bernhardpg): we might be able to get around this once we
+        # have SDP constraints over the edges
+        self.tau_F_1 = self.prog.NewContinuousVariables(1, "tau_F_1")[0]
+        self.tau_F_2 = self.prog.NewContinuousVariables(1, "tau_F_2")[0]
 
-    # angular acceleration
-    theta_ddot = (1 / robot.inertia) * (tau_F_1 + tau_F_2)
+        # linear acceleration
+        self.a_WB = (1 / robot.mass) * (self.f_F_1W + self.f_F_2W)
 
-    # torque = arm x force
-    p_BF_1W = p_BF_W + np.array([robot.foot_length / 2, 0])
-    p_BF_2W = p_BF_W - np.array([robot.foot_length / 2, 0])
-    tau_F_1 = cross_2d(p_BF_1W, f_F_1W)
-    tau_F_2 = cross_2d(p_BF_2W, f_F_2W)
+        # angular acceleration
+        self.theta_ddot = (1 / robot.inertia) * (self.tau_F_1 + self.tau_F_2)
 
-    relaxed_prog = MakeSemidefiniteRelaxation(prog)
+        # torque = arm x force
+        self.p_BF_1W = self.p_BF_W + np.array([robot.foot_length / 2, 0])
+        self.p_BF_2W = self.p_BF_W - np.array([robot.foot_length / 2, 0])
+        self.tau_F_1 = cross_2d(self.p_BF_1W, self.f_F_1W)
+        self.tau_F_2 = cross_2d(self.p_BF_2W, self.f_F_2W)
 
-    spectrahedron = Spectrahedron(relaxed_prog)
+        # TODO(bernhardpg): Friction cone must be formulated differently
+        # when we have tilted ground
+        mu = 0.5
+        for f in (self.f_F_1W, self.f_F_2W):
+            self.prog.AddLinearConstraint(f[1] >= 0)
+            self.prog.AddLinearConstraint(f[0] <= mu * f[1])
+            self.prog.AddLinearConstraint(f[0] >= -mu * f[1])
 
-    return spectrahedron
+        # TODO(bernhardpg): Step span limit
+
+    def get_state(self) -> npt.NDArray:
+        return np.concatenate([self.p_WB, [self.theta_WB], self.v_WB, [self.omega_WB]])
+
+    def get_input(self) -> npt.NDArray:
+        return np.concatenate([self.f_F_1W, self.f_F_2W, self.p_BF_W])
+
+    def get_dynamics(self) -> npt.NDArray:
+        return np.concatenate(
+            [self.v_WB, [self.omega_WB], self.a_WB, [self.theta_ddot]]
+        )
+
+    def get_convex_set(self) -> Spectrahedron:
+        relaxed_prog = MakeSemidefiniteRelaxation(self.prog)
+        spectrahedron = Spectrahedron(relaxed_prog)
+        return spectrahedron
 
 
 def main():
@@ -136,7 +167,13 @@ def main():
 
     robot = Robot()
 
-    spect = create_set(initial_stone, robot)
+    point_1 = KnotPoint(initial_stone, robot, name="1")
+    point_2 = KnotPoint(initial_stone, robot, name="2")
+
+    gcs = GraphOfConvexSets()
+
+    for point in (point_1, point_2):
+        gcs.AddVertex(point.get_convex_set(), name=point.name)
 
     # terrain.plot()
     # plt.show()
