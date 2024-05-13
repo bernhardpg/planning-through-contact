b8688839ac57156fec692fd4360b047e36ca719b
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2024-05-13 14:37:56 +0200

    Update test

--- tests/planning/footstep_planning/test_footstep_planner.py
+++ tests/planning/footstep_planning/test_footstep_planner.py
@@ -197,7 +197,8 @@ def test_footstep_planning_many_stones_lp_approx() -> None:
     )
 
 
-# Unfinished!
+# Unfinished! This is meant to be a WIP on a cutting-plane algorithm on
+# lp approximation
 @pytest.mark.skip
 def test_semidefinite_relaxation_lp_approximation() -> None:
     terrain = InPlaneTerrain()
@@ -327,27 +328,42 @@ def test_make_segments_per_terrain() -> None:
         target_stone_name="initial",
     )
     segments = planner._make_segments_for_terrain()
+    DEBUG = True
+
+    if DEBUG:
+        planner.create_graph_diagram("test_make_segments_diagram")
 
     # we should have one segment per stone
     assert len(segments) == len(terrain.stepping_stones)
-    # Make sure we have the correct number of steps given the step length
-    # TODO this is outdated
-    assert len(segments[0]) == int(np.floor(stone.width / step_span) + 2) * 2
+
+    def _expected_num_steps(_stone):
+        return (
+            planner._calc_num_steps_required_per_stone(_stone.width, step_span) * 2 - 1
+        )
+
+    # We add 2 here, as the "start_stance" and "final_stance" are added to the first and last segments,
+    # which in this case is the same
+    assert len(segments[0]) == _expected_num_steps(stone) + 2
 
     # Add another stone and try again
 
-    stone_2 = terrain.add_stone(x_pos=3.0, width=2.0, z_pos=0.2, name="initial")
+    stone_2 = terrain.add_stone(x_pos=3.0, width=2.0, z_pos=0.2, name="stone_2")
     planner = FootstepPlanner(
         cfg,
         terrain,
         initial_pose,
         target_pose,
         initial_stone_name="initial",
-        target_stone_name="initial",
+        target_stone_name="stone_2",
     )
     segments = planner._make_segments_for_terrain()
 
+    if DEBUG:
+        planner.create_graph_diagram("test_make_segments_diagram_2")
+
     assert len(segments) == len(terrain.stepping_stones)
+
     # Make sure we have the correct number of steps given the step length
-    assert len(segments[0]) == int(np.floor(stone.width / step_span) + 2) * 2
-    assert len(segments[1]) == int(np.floor(stone.width / step_span) + 2) * 2
+    # We add 1 for both here, as the "start_stance" and "final_stance" are added to the first and last segments,
+    assert len(segments[0]) == _expected_num_steps(stone) + 1
+    assert len(segments[1]) == _expected_num_steps(stone) + 1
