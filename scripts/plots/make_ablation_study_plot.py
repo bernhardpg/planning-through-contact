0c8f509b9de7624a905fb1fbbb9c1b8e631264ae
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2024-05-13 15:27:15 +0200

    Save ablation study data to file

--- scripts/plots/make_ablation_study_plot.py
+++ scripts/plots/make_ablation_study_plot.py
@@ -25,52 +25,88 @@ study_folders = [main_folder + folder for folder in study_names]
 studies = [AblationStudy.load_from_folder(folder, 100) for folder in study_folders]
 
 
-for study in studies:
-    print(f"Num trajs: {len(study)}")
-    print(f"Binary flows success rate: {study.percentage_binary_flows_success}%")
-    print(f"Feasible success rate: {study.percentage_feasible_success}%")
-    print(
-        f"Mean optimality gap: {np.mean([gap for gap in study.optimality_gaps if gap is not None])}"
-    )
-    print(
-        f"Median optimality gap: {np.median([gap for gap in study.optimality_gaps if gap is not None])}"
-    )
-    print(
-        f"Std optimality gap: {np.std([gap for gap in study.optimality_gaps if gap is not None])}"
-    )
-    print("#####")
-    print(f"Mean solve time GCS relaxation: {np.mean(study.solve_times_gcs_relaxed)}")
-    print(f"Std solve time GCS relaxation: {np.std(study.solve_times_gcs_relaxed)}")
-    print(
-        f"Median solve time GCS relaxation: {np.median(study.solve_times_gcs_relaxed)}"
-    )
-    print(f"Max solve time GCS relaxation: {np.max(study.solve_times_gcs_relaxed)}")
-    print(f"Min solve time GCS relaxation: {np.min(study.solve_times_gcs_relaxed)}")
-    print("#####")
-    print(f"Mean solve time binary flows: {np.mean(study.solve_times_binary_flows)}")
-    print(f"Std solve time binary flows: {np.std(study.solve_times_binary_flows)}")
-    print(
-        f"Median solve time binary flows: {np.median(study.solve_times_binary_flows)}"
-    )
-    print(f"Max solve time binary flows: {np.max(study.solve_times_binary_flows)}")
-    print(f"Min solve time binary flows: {np.min(study.solve_times_binary_flows)}")
-    print("#####")
-    # print(f"Mean rounding time: {np.mean(study.total_rounding_times)}")
-    # print(f"Std rounding time: {np.std(study.total_rounding_times)}")
-    # print(f"Median rounding time: {np.median(study.total_rounding_times)}")
-    print(f"Infeasible runs::")
-    for name in study.get_infeasible_idxs():
-        print(name)
+for folder, study in zip(study_folders, studies):
+    with open(folder + "/data.txt", "w") as f:
 
-    print(f"Numerical difficulties runs::")
-    for name in study.get_numerical_difficulties_idxs():
-        print(name)
+        print(f"Num trajs: {len(study)}", file=f)
+        print(
+            f"Binary flows success rate: {study.percentage_binary_flows_success:.2f}%",
+            file=f,
+        )
+        print(
+            f"Feasible success rate: {study.percentage_feasible_success:.2f}%", file=f
+        )
+        print(
+            f"Mean optimality gap: {np.mean([gap for gap in study.optimality_gaps if gap is not None]):.2f} %",
+            file=f,
+        )
+        print(
+            f"Median optimality gap: {np.median([gap for gap in study.optimality_gaps if gap is not None]):.2f} %",
+            file=f,
+        )
+        print(
+            f"Std optimality gap: {np.std([gap for gap in study.optimality_gaps if gap is not None]):.2f} %",
+            file=f,
+        )
+        print("#####", file=f)
+        print(
+            f"Mean solve time GCS relaxation: {np.mean(study.solve_times_gcs_relaxed)}",
+            file=f,
+        )
+        print(
+            f"Std solve time GCS relaxation: {np.std(study.solve_times_gcs_relaxed)}",
+            file=f,
+        )
+        print(
+            f"Median solve time GCS relaxation: {np.median(study.solve_times_gcs_relaxed)}",
+            file=f,
+        )
+        print(
+            f"Max solve time GCS relaxation: {np.max(study.solve_times_gcs_relaxed)}",
+            file=f,
+        )
+        print(
+            f"Min solve time GCS relaxation: {np.min(study.solve_times_gcs_relaxed)}",
+            file=f,
+        )
+        print("#####", file=f)
+        print(
+            f"Mean solve time binary flows: {np.mean(study.solve_times_binary_flows)}",
+            file=f,
+        )
+        print(
+            f"Std solve time binary flows: {np.std(study.solve_times_binary_flows)}",
+            file=f,
+        )
+        print(
+            f"Median solve time binary flows: {np.median(study.solve_times_binary_flows)}",
+            file=f,
+        )
+        print(
+            f"Max solve time binary flows: {np.max(study.solve_times_binary_flows)}",
+            file=f,
+        )
+        print(
+            f"Min solve time binary flows: {np.min(study.solve_times_binary_flows)}",
+            file=f,
+        )
+        print("#####", file=f)
+        # print(f"Mean rounding time: {np.mean(study.total_rounding_times)}")
+        # print(f"Std rounding time: {np.std(study.total_rounding_times)}")
+        # print(f"Median rounding time: {np.median(study.total_rounding_times)}")
+        print(f"Infeasible runs::", file=f)
+        for name in study.get_infeasible_idxs():
+            print(name, file=f)
 
-    # print(f"Mean solve time feasible: {np.mean(study.solve_times_feasible)}")
-    # print(f"Std solve time feasible: {np.std(study.solve_times_feasible)}")
-    # print(f"Median solve time feasible: {np.median(study.solve_times_feasible)}")
-    # print(f"Max solve time feasible: {np.max(study.solve_times_feasible)}")
-    # print(f"Min solve time feasible: {np.min(study.solve_times_feasible)}")
+        print(f"Numerical difficulties runs::", file=f)
+        for name in study.get_numerical_difficulties_idxs():
+            print(name, file=f)
+
+        # print(f"Mean solve time feasible: {np.mean(study.solve_times_feasible)}")
+        # print(f"Std solve time feasible: {np.std(study.solve_times_feasible)}")
+        # print(f"Median solve time feasible: {np.median(study.solve_times_feasible)}")
+        # print(f"Max solve time feasible: {np.max(study.solve_times_feasible)}")
+        # print(f"Min solve time feasible: {np.min(study.solve_times_feasible)}")
 
 
 colors = [
