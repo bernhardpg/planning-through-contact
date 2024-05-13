f983fa70564e6d8a99492bffa853455bd7bb9e52
nepfaff <nepfaff@mit.edu>
2024-05-13 16:29:24 -0400

    Add script for creating arbitrary shape pickles

---
+++ scripts/create_arbitrary_shape_pickle.py
@@ -0,0 +1,30 @@
+import pickle
+import numpy as np
+
+def main():
+    output_file = "data/arbitrary_shape.pickle"
+    boxes = [
+        {
+            "name": "box",
+            "size": [0.3, 0.1, 0.05],
+            "transform": np.eye(4),
+        },
+        {
+            "name": "box",
+            "size": [0.15, 0.15001, 0.05],
+            "transform": np.array(
+                [
+                    [1.0, 0.0, 0.0, 0.15],
+                    [0.0, 1.0, 0.0, -0.125],
+                    [0.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 1.0],
+                ]
+            ),
+        },
+    ]
+
+    with open(output_file, 'wb') as f:
+        pickle.dump(boxes, f)
+
+if __name__ == '__main__':
+    main()
